8636b52ec503dbfd2fcae2def45a2f0667d538dc
Jason Matthew <jmatthew@squiz.net>
2023-11-24 16:00:47 +1000

    feat: added testing example

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
     collectCoverageFrom: ["./**/*.{js,jsx}"],
-    coveragePathIgnorePatterns: ["node_modules", "dist"],
+    coveragePathIgnorePatterns: ["node_modules", "dist", "global/css"],
     coverageDirectory: "coverage",
     testEnvironment: "jsdom",
     setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
--- packages/card/Card.test.jsx
+++ packages/card/Card.test.jsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import React from "react";
 import Card from "./Card";
 
@@ -18,5 +18,43 @@ describe("Card", () => {
         render(<Card data={testData} />);
         const article = document.querySelector("article");
         expect(article).toBeInTheDocument();
+    });
+
+    it("should render an image when passed an image url", () => {
+        render(
+            <Card data={{ ...testData, imageUrl: "https://example.com" }} />
+        );
+        const image = document.querySelector("img");
+        expect(image).toBeInTheDocument();
+    });
+
+    it("should render the correct image url when passed", () => {
+        const imageUrl = "https://example.com/";
+        render(<Card data={{ ...testData, imageUrl }} />);
+        const image = document.querySelector("img");
+        expect(image.src).toBe(imageUrl);
+    });
+
+    it("should render the correct image alt tag when passed", () => {
+        const imageUrl = "https://example.com/";
+        const imageAlt = "alt desc";
+        render(<Card data={{ ...testData, imageUrl, imageAlt }} />);
+        const image = document.querySelector("img");
+        expect(image.alt).toBe(imageAlt);
+    });
+
+    it("should render the taxonomy when passed", () => {
+        const taxonomy = "test taxonomy";
+        render(<Card data={{ ...testData, taxonomy }} />);
+        const taxonomyElement = screen.getByText(taxonomy);
+        expect(taxonomyElement).toBeInTheDocument();
+    });
+
+    it("should render the taxonomy with the correct link when passed", () => {
+        const taxonomy = "test taxonomy";
+        const taxonomyUrl = "https://example.com/";
+        render(<Card data={{ ...testData, taxonomy, taxonomyUrl }} />);
+        const taxonomyElement = screen.getByText(taxonomy);
+        expect(taxonomyElement.href).toBe(taxonomyUrl);
     });
 });
