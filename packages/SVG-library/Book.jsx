ff21a81b9e277f95c33d74f57854168b043ca2b7
Isaac Astley <iastley@squiz.net>
2023-12-01 12:36:29 +1100

    Slight refactor & organisational update actioned from code review.

---
+++ packages/SVG-library/Book.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+/**
+* Renders out the book SVG icon
+*
+* @return {JSX.Element}
+*/
+export default function Book () {
+    return(
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+            <path d="M10 5.21049V16.0438M10 5.21049C9.02675 4.56389 7.70541 4.1665 6.25 4.1665C4.79459 4.1665 3.47325 4.56389 2.5 5.21049V16.0438C3.47325 15.3972 4.79459 14.9998 6.25 14.9998C7.70541 14.9998 9.02675 15.3972 10 16.0438M10 5.21049C10.9732 4.56389 12.2946 4.1665 13.75 4.1665C15.2054 4.1665 16.5268 4.56389 17.5 5.21049V16.0438C16.5268 15.3972 15.2054 14.9998 13.75 14.9998C12.2946 14.9998 10.9732 15.3972 10 16.0438" stroke="#6D6C69" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+    );
+}
--- packages/card/verticalCard.jsx
+++ packages/card/verticalCard.jsx
@@ -1,113 +1,121 @@
 import React from "react";
-import { titleSize, typeIcon } from "./cardUtilities";
+import SVG from "../SVG-library/SVG";
 
 /**
- * Card package
- *
- * @param {string} title
- * The components title
- *
- * @param {string} description
- * The components description
- *
- * @param {string} liveUrl
- * The components url
- *
- * @param {string} imageUrl
- * The components image Url
- *
- * @param {string} imageAlt
- * The components image Alt
- *
- * @param {string} taxonomy
- * The components taxonomy value
- *
- * @param {string} taxonomyUrl
- * The components taxonomy url
- *
- * @param {string} type
- * The components type
- *
- * @returns {JSX.Element}
- * @constructor
- */
-export default function VerticalCard({
-  data,
-  cardSize,
-  showDescriptionOnMobile,
-  hideImages,
+* This function will return the appropriate heading font
+* sizes for the card's title.
+*
+* @param {string} size
+* The size can be one of the following:
+* - featured
+* - medium
+* - small
+*
+* @return {string}
+*/
+function titleSize (size) {
+    if (size === "featured") return "su-text-[35px] md:su-text-[40px] lg:su-text-[43px]";
+    else if (size === "medium") return "su-text-[21px] lg:su-text-[24px]";
+    else if (size === "small") return "su-text-[21px] lg:su-text-[24px]";
+}
+
+/**
+* Card package
+*
+* @param {string} title 
+* The components title
+* 
+* @param {string} description 
+* The components description
+*
+* @param {string} liveUrl 
+* The components url
+* 
+* @param {string} imageUrl 
+* The components image Url
+*
+* @param {string} imageAlt 
+* The components image Alt
+*
+* @param {string} taxonomy 
+* The components taxonomy value
+*
+* @param {string} taxonomyUrl 
+* The components taxonomy url
+*
+* @param {string} type 
+* The components type
+*
+* @returns {JSX.Element}
+* @constructor
+*/
+export default function VerticalCard({ 
+    data : {
+        title,
+        description,
+        liveUrl,
+        imageUrl,
+        imageAlt,
+        taxonomy,
+        taxonomyUrl,
+        type
+    }, 
+    cardSize, 
+    showDescriptionOnMobile, 
+    hideImages
 }) {
-  const {
-    title,
-    description,
-    liveUrl,
-    imageUrl,
-    imageAlt,
-    taxonomy,
-    taxonomyUrl,
-    type,
-  } = data;
+    const displayImages = hideImages === false;
 
-  // for small cards only
-  const descriptionOnMobile =
-    cardSize === "small" && showDescriptionOnMobile === false
-      ? " su-hidden lg:su-block "
-      : "";
+    // for small cards only
+    const descriptionOnMobile = cardSize === "small" && showDescriptionOnMobile === false ? "su-hidden lg:su-block" : "";
 
-  return (
-    <article
-      className="su-component-card su-relative su-w-full"
-      data-testid="vertical-card"
-    >
-      {hideImages === false && imageUrl && (
-        <div className="su-relative su-block su-aspect-[50/33] su-mb-[19px]">
-          <img
-            className="su-absolute su-object-cover su-object-center su-w-full su-h-full"
-            src={imageUrl}
-            alt={imageAlt}
-          />
-        </div>
-      )}
+    return (
+        <article className="su-component-card su-relative su-w-full">
+            { 
+                (displayImages && imageUrl) && (
+                    <div className="su-relative su-block su-aspect-[50/33] su-mb-[19px]">
+                        <img
+                            className="su-absolute su-object-cover su-object-center su-w-full su-h-full"
+                            src={imageUrl}
+                            alt={imageAlt}
+                        />
+                    </div>
+                )
+            }
 
-      {taxonomy && (
-        <p className="su-relative su-z-10 su-text-[16px] lg:su-text-[18px] su-leading-[20.8px] lg:su-leading-[23.4px] su-mb-[13px] su-font-semibold">
-          <a
-            href={taxonomyUrl}
-            className="focus:su-outline-0 focus:su-ring su-text-digital-red su-no-underline hover:su-text-digital-red dark:su-text-dark-mode-red hover:dark:su-text-dark-mode-red"
-          >
-            {taxonomy}
-          </a>
-        </p>
-      )}
+            {taxonomy && (
+                <p className="su-relative su-z-10 su-text-[16px] lg:su-text-[18px] su-leading-[20.8px] lg:su-leading-[23.4px] su-mb-[13px] su-font-semibold">
+                    <a
+                        href={taxonomyUrl}
+                        className="focus:su-outline-0 focus:su-ring su-text-digital-red su-no-underline hover:su-text-digital-red dark:su-text-dark-mode-red hover:dark:su-text-dark-mode-red"
+                    >
+                        {taxonomy}
+                    </a>
+                </p>
+            )}
 
-      <div className="su-flex su-flex-wrap su-gap-[9px]">
-        <h2
-          className={`su-w-full ${titleSize(
-            cardSize
-          )} su-leading-[25.2px] lg:su-leading-[28.8px] su-font-serif su-my-0`}
-        >
-          <a
-            href={liveUrl}
-            className="focus:su-outline-0 focus:before:su-ring hover:su-text-digital-red su-transition su-text-black dark:su-text-white dark:hover:su-text-dark-mode-red before:su-absolute before:su-w-full before:su-h-full before:su-block before:su-top-0 before:su-left-0"
-          >
-            {title}
-          </a>
-        </h2>
+            <div className="su-flex su-flex-wrap su-gap-[9px]">
+                <h2 className={ `su-w-full ${ titleSize(cardSize) } su-leading-[25.2px] lg:su-leading-[28.8px] su-font-serif su-my-0` }>
+                    <a
+                        href={liveUrl}
+                        className="focus:su-outline-0 focus:before:su-ring hover:su-text-digital-red su-transition su-text-black dark:su-text-white dark:hover:su-text-dark-mode-red before:su-absolute before:su-w-full before:su-h-full before:su-block before:su-top-0 before:su-left-0"
+                    >
+                        {title}
+                    </a>
+                </h2>
 
-        {type && (
-          <p className="su-flex su-font-semibold su-text-black-70 dark:su-text-black-60 su-my-0 su-gap-[6px] su-flex-nowrap su-items-center su-text-[16px] su-leading-[20.8px]">
-            {typeIcon(type)}
+                {type && (
+                    <p className="su-flex su-font-semibold su-text-black-70 dark:su-text-black-60 su-my-0 su-gap-[6px] su-flex-nowrap su-items-center su-text-[16px] su-leading-[20.8px]">
+                        <SVG type={ type } />
 
-            <span>{type}</span>
-          </p>
-        )}
+                        <span>{type}</span>
+                    </p>
+                )}
 
-        <p
-          className={`su-mb-0${descriptionOnMobile}su-text-[19px] su-leading-[23.75px]`}
-        >
-          {description}
-        </p>
-      </div>
-    </article>
-  );
+                <p className={ `su-mb-0 ${ descriptionOnMobile } su-text-[19px] su-leading-[23.75px]` }>
+                    { description }
+                </p>
+            </div>
+        </article>
+    );
 }
