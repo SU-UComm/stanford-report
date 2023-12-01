21f818555ffc1af6a088c1846f9fe57e0a22e0c9
Isaac Astley <iastley@squiz.net>
2023-12-01 12:59:39 +1100

    Reversing changes to component files

--- components/featured-content/Component.jsx
+++ components/featured-content/Component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 // these are our specific templates for the component.
 import Card from "../../packages/card/Card";
-import { LinkedHeading } from "../../packages/headings/Heading";
+import Heading from "../../packages/headings/Heading";
 
 const mapCardDataFromFunnelbackResults = ({ title, listMetadata, liveUrl }) => {
   const description =
@@ -49,77 +49,21 @@ const mapCardDataFromFunnelbackResults = ({ title, listMetadata, liveUrl }) => {
 export default function FeaturedContent({ title, ctaText, ctaUrl, data }) {
   return (
     <div className="su-w-full su-component-multicolumn">
-      <LinkedHeading title={title} ctaText={ctaText} ctaUrl={ctaUrl} />
+      <Heading title={title} ctaText={ctaText} ctaUrl={ctaUrl} />
 
       {data &&
         data.response &&
         data.response.resultPacket &&
         data.response.resultPacket.results && (
           <div className="su-relative su-flex su-flex-wrap md:su-flex-nowrap su-flex-1 su-gap-[68px] md:su-gap-[72px] lg:su-gap-[102px] su-place-content-between">
-                <Card
-                    data={{
-                        "title": "Seeing the oceans in a new light",
-                        "description": "Halleh Balch, an experimental physicist in the Dionne lab at Stanford, has developed a thumbnail-sized optical sensor that can track the health of marine ecosystems in near-real time through quick detection of environmental DNA. It could be a critical tool for natural resource managers in the face of climate change impacts like coral bleaching, warming seas, and migration of species.",
-                        "liveUrl": "#",
-                        "imageUrl": "https://picsum.photos/500/330",
-                        "imageAlt": "Lorem ipsum dolor sit amet",
-                        "taxonomy": "Earth & Climate",
-                        "taxonomyUrl": "#",
-                        "type": "Article"
-                    }}
-                    cardType="vertical"
-                    cardSize="featured"
-                    showDescriptionOnMobile={ false }
-                    hideImages={ false }
-                />
-
-                <Card
-                    data={{
-                        "title": "Jenny Martinez, Stanford's new provost, on work and whimsy",
-                        "liveUrl": "#",
-                        "description": "Halleh Balch, an experimental physicist in the Dionne lab at Stanford, has developed a thumbnail-sized optical sensor that can track the health of marine ecosystems in near-real time through quick detection of environmental DNA. It could be a critical tool for natural resource managers in the face of climate change impacts like coral bleaching, warming seas, and migration of species.",
-                        "imageUrl": "https://picsum.photos/500/330",
-                        "imageAlt": "Lorem ipsum dolor sit amet",
-                        "taxonomy": "Leadership & Governance",
-                        "taxonomyUrl": "#",
-                        "type": "Video"
-                    }}
-                    cardType="vertical"
-                    cardSize="small"
-                    showDescriptionOnMobile={ false }
-                    hideImages={ false }
-                />
-
-                <Card
-                    data={{
-                        "title": "Meet the robot that can sort your random stuff",
-                        "liveUrl": "#",
-                        "description": "Halleh Balch, an experimental physicist in the Dionne lab at Stanford, has developed a thumbnail-sized optical sensor that can track the health of marine ecosystems in near-real time through quick detection of environmental DNA. It could be a critical tool for natural resource managers in the face of climate change impacts like coral bleaching, warming seas, and migration of species.",
-                        "imageUrl": "https://picsum.photos/500/330",
-                        "imageAlt": "Lorem ipsum dolor sit amet",
-                        "taxonomy": "Science & Engineering",
-                        "taxonomyUrl": "#",
-                        "type": "Q & A"
-                    }}
-                    cardType="vertical"
-                    cardSize="small"
-                    showDescriptionOnMobile={ false }
-                    hideImages={ false }
-                />
-            </div>
+            {data.response.resultPacket.results.map((result) => (
+              <Card
+                key={result.rank}
+                data={mapCardDataFromFunnelbackResults(result)}
+              />
+            ))}
+          </div>
         )}
     </div>
   );
 }
-
-                //{
-                    //data.response.resultPacket.results.map((result) => { 
-                        //return (
-                            //<Card
-                                //key={result.rank}
-                                //data={mapCardDataFromFunnelbackResults(result)}
-                                //cardType="vertical"
-                            ///>
-                        //) 
-                    //})
-                //}
