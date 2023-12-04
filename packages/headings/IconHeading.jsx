0810be4d2b3b9b7fe34ce162415149e7f15cb1a8
Hannah Malcolm <hmalcolm@squiz.net>
2023-12-04 16:30:41 +1000

    Add beginning of icon heading

--- packages/headings/IconHeading.jsx
+++ packages/headings/IconHeading.jsx
@@ -1,25 +1,29 @@
 import React from "react";
 
 /**
- * Renders out the linked heading, as seen here: https://www.figma.com/file/Fxe0NRKM09lCA3oCkHXHwB/FINAL----Landing-Pages----Home?type=design&node-id=1440-21954&mode=dev
+ * Renders out the icon heading
  *
  * @param {string} title
  * The title text
  *
- * @param {string} icon
- * The icon to precede the title
+ * @param {string} icons
+ * Object containing "light" and "dark" icons
  *
  * @param {string} headingSize
  * The semantic heading tag
  *
  * @return {JSX.element}
  */
-export function IconHeading({ title, icon, headingSize = "h2" }) {
+export function IconHeading({ title, icons = {}, headingSize = "h2" }) {
   const Tag = headingSize;
   return (
     <div>
       <Tag className="su-flex su-flex-wrap su-gap-[6px] su-my-0 su-text-[18px] su-text-black-90 dark:su-text-white su-font-semibold su-font-sans">
-        {title}
+        {icons.light && <span className="dark:su-hidden">{icons.light}</span>}
+        {icons.dark && (
+          <span className="su-hidden dark:su-block">{icons.dark}</span>
+        )}
+        <span>{title}</span>
       </Tag>
     </div>
   );
