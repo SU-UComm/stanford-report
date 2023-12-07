cd89df3b7dde78daaa8df43f83e14a6e94b59009
Hannah Malcolm <hmalcolm@squiz.net>
2023-12-07 14:46:04 +1000

    Update avatar test

--- packages/quotes/Avatar.test.jsx
+++ packages/quotes/Avatar.test.jsx
@@ -23,24 +23,24 @@ describe("Avatar", () => {
   it("should render the component at the large size", () => {
     render(<Avatar image={testData.image} avatarSize="large" />);
     const element = document.querySelector(".su-component-avatar");
-    const largeSize = document.querySelector('[data-test="size-large"]');
+    const avatar = document.querySelector('[data-test="size-large"]');
     expect(element).toBeInTheDocument();
-    expect(largeSize).toBeInTheDocument();
+    expect(avatar).toBeInTheDocument();
   });
 
   it("should render the component at the medium size", () => {
     render(<Avatar image={testData.image} avatarSize="medium" />);
     const element = document.querySelector(".su-component-avatar");
-    const largeSize = document.querySelector('[data-test="size-medium"]');
+    const avatar = document.querySelector('[data-test="size-medium"]');
     expect(element).toBeInTheDocument();
-    expect(largeSize).toBeInTheDocument();
+    expect(avatar).toBeInTheDocument();
   });
 
   it("should render the component at the small size", () => {
     render(<Avatar image={testData.image} avatarSize="small" />);
     const element = document.querySelector(".su-component-avatar");
-    const largeSize = document.querySelector('[data-test="size-small"]');
+    const avatar = document.querySelector('[data-test="size-small"]');
     expect(element).toBeInTheDocument();
-    expect(largeSize).toBeInTheDocument();
+    expect(avatar).toBeInTheDocument();
   });
 });
