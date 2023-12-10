991ac8024ce3dd0d13c740944af9a363837a91d6
Hannah Malcolm <hmalcolm@squiz.net>
2023-12-11 09:48:28 +1000

    Change multicolumn grid to use maps

--- packages/grids/MultiColumnGridContent.jsx
+++ packages/grids/MultiColumnGridContent.jsx
@@ -19,26 +19,34 @@ export function MultiColumnGridContent({
   const NOT_FIRST_COLUMN = placement !== 0;
   const HAS_SEPARATOR = separator === true;
 
-  const widthClasses = {
-    "2col": "md:su-basis-1/2",
-    "3col": "md:su-basis-1/3",
-  };
-
-  const separatorClasses = {
-    "2col":
-      "before:su-w-full before:md:su-w-[1px] before:su-absolute before:su-bg-black-30 dark:before:su-bg-black before:su-h-[1px] before:md:su-h-full before:su-left-0 before:su-top-[-34px] before:md:su-top-0 before:md:su-left-[-36px] before:lg:su-left-[-80px]",
-    "3col":
-      "before:su-w-full before:md:su-w-[1px] before:su-absolute before:su-bg-black-30 dark:before:su-bg-black before:su-h-[1px] before:md:su-h-full before:su-left-0 before:su-top-[-34px] before:md:su-top-0 before:md:su-left-[-36px] before:lg:su-left-[-51px]",
-  };
+  const widthClasses = new Map();
+  widthClasses.set(
+    "2col",
+    "md:su-basis-1/2"
+  );
+  widthClasses.set(
+    "3col",
+    "md:su-basis-1/3"
+  );
+  
+  const separatorClasses = new Map();
+  separatorClasses.set(
+    "2col",
+    "before:su-w-full before:md:su-w-[1px] before:su-absolute before:su-bg-black-30 dark:before:su-bg-black before:su-h-[1px] before:md:su-h-full before:su-left-0 before:su-top-[-34px] before:md:su-top-0 before:md:su-left-[-36px] before:lg:su-left-[-80px]"
+  );
+  separatorClasses.set(
+    "3col",
+    "before:su-w-full before:md:su-w-[1px] before:su-absolute before:su-bg-black-30 dark:before:su-bg-black before:su-h-[1px] before:md:su-h-full before:su-left-0 before:su-top-[-34px] before:md:su-top-0 before:md:su-left-[-36px] before:lg:su-left-[-51px]"
+  );
 
   return (
     <div
       data-test={`column-${placement}`}
       className={[
         "su-relative su-flex-grow",
-        widthClasses[`${totalColumns}col`],
+        widthClasses.get(`${totalColumns}col`),
         HAS_SEPARATOR && NOT_FIRST_COLUMN
-          ? separatorClasses[`${totalColumns}col`]
+          ? separatorClasses.get(`${totalColumns}col`)
           : "",
       ].join(" ")}
     >
