95070d88bc934691ff94bcc6fe1bf22e96e1b341
Isaac Astley <iastley@squiz.net>
2023-12-11 17:58:41 +1100

    First pass at the multicolumn listing component

---
+++ components/multicolumn-listing/Component.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+// these are our specific templates for the component.
+import Card from "../../packages/card/Card";
+import { LinkedHeading } from "../../packages/headings/Heading";
+import { MultiColumnGrid } from "../../packages/grids/Grids";
+import { Avatar, PullQuote } from "../../packages/quotes/Quotes";
+
+/**
+ * Featured content component
+ *
+ * @param {string} title The component title
+ * @param {string} ctaText The cta text
+ * @param {string} ctaUrl The cta url
+ * @param {object} data JSON data for content
+ * ... any other options needed
+ * @returns {JSX.Element}
+ * @constructor
+ */
+
+export default function MulticolumnListing({
+  title,
+  ctaText,
+  ctaUrl,
+  data,
+  alignment,
+  displayThumbnails,
+  displayDescriptions,
+  featuredDescription,
+  numberOfCards,
+}) {
+  const featuredCardData = data[0];
+  if (featuredDescription !== "" && featuredDescription !== null) {
+    featuredCardData.description = featuredDescription;
+  }
+
+  const cards = [];
+
+  data.forEach((cardData, i) => {
+    if (i <= numberOfCards - 1) {
+      cards.push(
+        <Card
+          data={cardData}
+          displayDescription={displayDescriptions}
+          displayThumbnail={displayThumbnails}
+          cardSize="small"
+        />
+      );
+    }
+  });
+
+  return (
+    <>
+      <LinkedHeading title={title} ctaText={ctaText} ctaUrl={ctaUrl} />
+
+      <MultiColumnGrid alignment={alignment} items={cards} />
+    </>
+  );
+}
