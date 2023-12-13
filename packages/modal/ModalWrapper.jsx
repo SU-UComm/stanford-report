026566f9a62d2c1cd066dfcc8619705347d77232
Hannah Malcolm <hmalcolm@squiz.net>
2023-12-13 15:42:35 +1000

    Add beginning modal package

---
+++ packages/modal/ModalWrapper.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { OverlayContainer } from "@react-aria/overlays";
+import { FocusScope } from "@react-aria/focus";
+// import styles from "./modal.module.scss";
+import Modal from "./ModalContent";
+
+/**
+ * React implementation of the Plug and Play Modal, uses the Adobe @react-aria package
+ * to handle the modal / dialog related accessability concerns such as focus movement, focus traps,
+ * ESC to close ect.
+ *
+ * Needs to be wrapped within a <OverlayProvider> to aria hide the application when the modal opens.
+ *
+ * Comments on @react-aria hooks come directly from their documentation.
+ */
+/**
+ * A function to generate the Modal.
+ * @param props The props object.
+ * @param props.titleId A string to define the modal in the aria-describedby field.
+ * @param props.title The heading to add to the Modal.
+ * @param props.onClose A callback function to run on the onClose event.
+ * @param props.children A reactNode to add and render out.
+ * @param props.className A custom className to add into the markup.
+ * @param props.mobileFullScreen A control to make the modal go full width on mobile devices.
+ * @returns {JSX.Element}
+ */
+export function ModalWrapper({
+  titleId,
+  title,
+  onClose,
+  children,
+  mobileFullScreen,
+}) {
+  return (
+    <OverlayContainer>
+      <div>
+        <FocusScope contain restoreFocus autoFocus>
+          <Modal titleId={titleId} title={title} onClose={onClose}>
+            {children}
+          </Modal>
+        </FocusScope>
+      </div>
+    </OverlayContainer>
+  );
+}
+
+export default ModalWrapper;
