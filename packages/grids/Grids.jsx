c51af1f49593f0319055575a096d83649e1bb1ab
Hannah Malcolm <hmalcolm@squiz.net>
2024-01-10 11:46:10 +1000

    Add base layouts for previews and wrap components

--- packages/grids/Grids.jsx
+++ packages/grids/Grids.jsx
@@ -1,3 +1,4 @@
+export { Container } from "./Container";
 export { FeaturedGrid } from "./FeaturedGrid";
 export { FeaturedGridContent } from "./FeaturedGridContent";
 export { MultiColumnGrid } from "./MultiColumnGrid";
