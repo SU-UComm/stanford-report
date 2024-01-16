6452a55222d7a3d015236701c8b8de0cc3e79b89
Hannah Malcolm <hmalcolm@squiz.net>
2024-01-16 16:54:09 +1000

    Update fonts to use Source Sans 3 and Source Serif 4

--- global/css/plugins/theme/fontFamily.js
+++ global/css/plugins/theme/fontFamily.js
@@ -5,14 +5,14 @@ module.exports = function () {
   return {
     mono: ['"Roboto Mono"', "Menlo", '"Courier New"', "monospace"],
     sans: [
-      '"Source Sans Pro"',
+      '"Source Sans 3"',
       '"Helvetica Neue"',
       "Helvetica",
       "Arial",
       "sans-serif",
     ],
     serif: [
-      '"Source Serif Pro"',
+      '"Source Serif 4"',
       "Georgia",
       "Times",
       '"Times New Roman"',
@@ -21,7 +21,7 @@ module.exports = function () {
     slab: ['"Roboto Slab"', "Georgia", "Times", '"Times New Roman"', "serif"],
     stanford: [
       "Stanford",
-      '"Source Serif Pro"',
+      '"Source Serif 4"',
       "Georgia",
       "Times",
       '"Times New Roman"',
