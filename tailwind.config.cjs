03ba0a0c8dc1211b14081239aedbf0b5bb741c74
Anthony Ponomarenko <aponomarenko@squiz.net>
2024-01-18 10:30:31 +1000

    added component overrides

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -23,19 +23,15 @@ module.exports = {
     // decanter: require(`${dir}/theme/decanter.js`)(),
     // Decanter's extension of Tailwind's default theme.
     extend: {
-      // borderRadius: require(`${dir}/theme/borderRadius.js`)(),
-      // borderWidth: require(`${dir}/theme/borderWidth.js`)(),
+      borderRadius: require(`${dir}/theme/borderRadius.js`)(),
+      borderWidth: require(`${dir}/theme/borderWidth.js`)(),
       colors: require(`${dir}/theme/colors.js`)(),
-      // fontFamily: require(`${dir}/theme/fontFamily.js`)(),
-      // fontSize: require(`${dir}/theme/fontSize.js`)(),
-      // gap: require(`${dir}/theme/gap.js`)(),
-      // lineHeight: require(`${dir}/theme/lineHeight.js`)(),
-      // maxWidth: require(`${dir}/theme/maxWidth.js`)(),
-      // screens: require(`${dir}/theme/screens.js`)(),
-      // spacing: require(`${dir}/theme/spacing.js`)(),
-      // fontWeight: require(`${dir}/theme/fontWeight.js`)(),
-      // lineClamp: require(`${dir}/theme/lineClamp.js`)(),
-      // transitionDuration: require(`${dir}/theme/transitionDuration.js`)(),
+      fontFamily: require(`${dir}/theme/fontFamily.js`)(),
+      fontSize: require(`${dir}/theme/fontSize.js`)(),
+      gap: require(`${dir}/theme/gap.js`)(),
+      lineHeight: require(`${dir}/theme/lineHeight.js`)(),
+      maxWidth: require(`${dir}/theme/maxWidth.js`)(),
+      spacing: require(`${dir}/theme/spacing.js`)(),
     },
   },
 
@@ -65,37 +61,16 @@ module.exports = {
     // 3rd Party Plugins;
     require("@tailwindcss/aspect-ratio"),
     require("@tailwindcss/forms"),
-    // require('@tailwindcss/line-clamp'),
-
-    // @tailwind base;
-    // require(`${dir}/base/base.js`)(),
 
     // @tailwind components;
-    /*
     require(`${dir}/components/form/input.js`)(),
     require(`${dir}/components/form/buttons.js`)(),
     require(`${dir}/components/form/form-elements.js`)(),
     require(`${dir}/components/layout/centered-container.js`)(),
     require(`${dir}/components/layout/grid-gap.js`)(),
     require(`${dir}/components/link/stretched-link.js`)(),
-    require(`${dir}/components/lists/lists.js`)(),
     require(`${dir}/components/logo/logo.js`)(),
-    require(`${dir}/components/media/embed-container.js`)(),
     require(`${dir}/components/responsive-spacing/responsive-spacing.js`)(),
-    require(`${dir}/components/skiplink/skiplink.js`)(),
-    require(`${dir}/components/tables/borderless.js`)(),
-    require(`${dir}/components/typography/modular-typography.js`)(),
-    require(`${dir}/components/typography/styles.js`)(),
     require(`${dir}/components/typography/wysiwyg.js`)(),
-    require(`${dir}/components/shadow/text-shadow.js`)(),
-
-    // @tailwind utilities;
-    require(`${dir}/utilities/accessibility/accessibility-hidden.js`)(),
-    require(`${dir}/utilities/backface-visibility/backface-visibility.js`)(),
-    require(`${dir}/utilities/link/link.js`)(),
-    require(`${dir}/utilities/link/link-underline.js`)(),
-    require(`${dir}/utilities/link/link-fontweight.js`)(),
-    require(`${dir}/utilities/typography/break-words.js`)(),
-    require(`${dir}/utilities/typography/writing-mode.js`)(), */
   ],
 };
