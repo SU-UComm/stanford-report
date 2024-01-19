25d6b51f2b65eae9baafedcf5ce53de6e02c3271
Isaac Astley <iastley@squiz.net>
2024-01-19 16:13:19 +1100

    attempt to allow image assets for the pullquote by using the basicAssetUri

--- components/pullquote/server.jsx
+++ components/pullquote/server.jsx
@@ -1,21 +1,23 @@
 import renderComponent from "../../packages/utils/render-component";
 import Component from "./Component";
 import CardDataAdapter from "../../packages/utils/CardDataAdapter";
-import MatrixCardService from "../../packages/utils/MatrixCardService";
+import basicAssetUri from "../../packages/utils/basicAssetUri";
 
 export default async (args, info) => {
   const { API_IDENTIFIER } = info.set.environment;
   const { ctx } = info;
   const adapter = new CardDataAdapter();
-  const matrixAsset = [{ cardAsset: args.displayConfiguration.asset }];
+  // const matrixAsset = [{ cardAsset: args.displayConfiguration.asset }];
+  const matrixAsset = args.displayConfiguration.asset;
   let data = null;
 
   if (matrixAsset) {
-    const service = new MatrixCardService({ ctx, API_IDENTIFIER });
+    data = await basicAssetUri(ctx, matrixAsset);
+    // const service = new MatrixCardService({ ctx, API_IDENTIFIER });
 
-    adapter.setCardService(service);
+    // adapter.setCardService(service);
 
-    data = await adapter.getCards(matrixAsset);
+    // data = await adapter.getCards(matrixAsset);
   }
 
   const renderProps = {
