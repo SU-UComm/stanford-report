486aa59ae3bdca333cfc372b191f1c6a5a61eae9
Isaac Astley <iastley@squiz.net>
2024-01-24 16:49:55 +1100

    wip on the single featured content component

---
+++ components/single-featured-content/Component.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+// import specific templates for the component
+import { Container } from "../../packages/grids/Container";
+import { LinkedHeading } from "../../packages/headings/Heading";
+
+/**
+ * Base component
+ *
+ * @param {string} title The component title
+ * ... any other options needed
+ * @returns {JSX.Element}
+ * @constructor
+ */
+
+export default function SingleFeaturedContent({
+  contentConfiguration,
+  displayConfiguration,
+  data,
+  headingData,
+}) {
+  if (
+    contentConfiguration.featuredDescription !== "" &&
+    contentConfiguration.featuredDescription !== null &&
+    contentConfiguration.featuredDescription !== undefined
+  ) {
+    featuredCardData.description = contentConfiguration.featuredDescription;
+  }
+
+  return (
+    <Container>
+      <LinkedHeading
+        title={headingData.title}
+        ctaText={headingData.ctaText}
+        ctaUrl={headingData.resolvedUrl}
+      />
+    </Container>
+  );
+}
