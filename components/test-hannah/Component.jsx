bebd92a84151941250e1c3e02c7ea79fb53d1a7c
Hannah Malcolm <hmalcolm@squiz.net>
2024-02-01 17:17:14 +1000

    Add some basic styles

--- components/test-hannah/Component.jsx
+++ components/test-hannah/Component.jsx
@@ -20,16 +20,28 @@ export default function TestHannah({
   textPosition,
 }) {
   return (
-    <Container>
-      <div>
-        <blockquote>
-          {quote}
-          <cite>
-            <span className="su-block">{attribution}</span>
-            <span className="su-block">{yearLabel}</span>
-          </cite>
-        </blockquote>
-      </div>
-    </Container>
+    <section
+      className="su-relative su-bg-cover su-bg-no-repeat su-py-[100px] su-text-white before:su-bg-black-true before:su-w-full before:su-h-full before:su-top-0 before:su-left-0 before:su-absolute before:su-opacity-25 before:su-z-10"
+      style={{
+        backgroundImage: `url(${image})`,
+      }}
+    >
+      <Container>
+        <div
+          className={[
+            "su-relative su-z-20 md:su-max-w-[674px]",
+            textPosition === "right" ? "su-ml-auto" : "su-ml-0",
+          ].join(" ")}
+        >
+          <blockquote className="su-type-2 su-font-serif">
+            {quote}
+            <cite className="su-text-23 su-block su-font-sans su-mt-[28px]">
+              <span className="su-block">{attribution}</span>
+              <span className="su-block">{yearLabel}</span>
+            </cite>
+          </blockquote>
+        </div>
+      </Container>
+    </section>
   );
 }
