5e776384483ef888f812a2db50ffc1343f5a79b5
Yvonne Tang <ytang2@stanford.edu>
2024-02-07 15:53:05 -0800

    Add 2 icons; add nvmrc

--- components/interactive-photo-card/Component.jsx
+++ components/interactive-photo-card/Component.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { cnb } from "cnbuilder";
 
 import { Container } from "../../packages/grids/Container";
+import Flip from "../../packages/SVG-library/Flip";
+import Plus from "../../packages/SVG-library/Plus";
 
 /**
  * Interactive Photo Card component
@@ -20,7 +22,7 @@ export default function InteractivePhotoCard({
   imageAlignment,
 }) {
   return (
-    <Container>
+    <div className="su-cc">
       <article
         className={cnb(
           "su-relative su-flex su-gap-20",
@@ -29,23 +31,33 @@ export default function InteractivePhotoCard({
             : "su-flex-col-reverse lg:su-flex-row-reverse"
         )}
       >
-        <div className="su-basis-1/2">
-          <div className="su-rounded-[8px] su-bg-white">
+        <div className="su-basis-1/2 su-rs-px-5 su-rs-pt-6 su-rs-pb-4 su-shadow-lg su-rounded-[8px]">
+          <div className="su-bg-white">
             {eyebrow && (
               <div className="su-type-1 su-text-black-60 su-font-semibold su-rs-mb-1">
                 {eyebrow}
               </div>
             )}
-            <h2 className="su-type-4 su-font-bold su-font-sans su-type">
+            <h3 className="su-type-4 su-font-bold su-font-sans su-type su-rs-mb-0">
               {title}
-            </h2>
+            </h3>
+            <button
+              type="button"
+              aria-label={`Read more about ${title}`}
+              className="su-block su-ml-auto su-mr-0 su-bg-black su-text-white su-rounded-full su-p-10"
+            >
+              <Plus className="su-size-50 su-fill-none" />
+            </button>
           </div>
-          <paragraph className="su-big-paragraph">{content}</paragraph>
+          <div className="su-bg-cardinal-red su-text-white su-hidden">
+            <paragraph className="su-big-paragraph">{content}</paragraph>
+            <Flip className="su-size-36 su-fill-none" />
+          </div>
         </div>
-        <div className="su-rounded-[8px] su-overflow-hidden su-basis-1/2">
+        <div className="su-rounded-[8px] su-overflow-hidden su-basis-1/2 su-shadow-lg">
           <img src={imageUrl} alt="" className="su-object-cover su-size-full" />
         </div>
       </article>
-    </Container>
+    </div>
   );
 }
---
+++ packages/SVG-library/Plus.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+/**
+ * Renders out the plus icon for the front side of the Interactive Photo Cards
+ *
+ * @return {JSX.Element}
+ */
+export default function Flip({ className }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={className}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 4.5v15m7.5-7.5h-15"
+      />
+    </svg>
+  );
+}
