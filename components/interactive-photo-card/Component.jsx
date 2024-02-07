a3cc021c20ed481e433ab9ab09e3ab8d03b0ad24
Yvonne Tang <ytang2@stanford.edu>
2024-02-07 14:23:47 -0800

    component markup and some styles

--- components/interactive-photo-card/Component.jsx
+++ components/interactive-photo-card/Component.jsx
@@ -18,5 +18,26 @@ export default function InteractivePhotoCard({
   imageUrl,
   imageAlignment,
 }) {
-  return <Container>{title}</Container>;
+  return (
+    <Container>
+      <article className="su-relative su-grid lg:su-grid-cols-2 su-gap-20">
+        <div>
+          <div className="su-rounded-[8px] su-bg-white">
+            {eyebrow && (
+              <div className="su-type-1 su-text-black-60 su-font-semibold su-rs-mb-1">
+                {eyebrow}
+              </div>
+            )}
+            <h2 className="su-type-4 su-font-bold su-font-sans su-type">
+              {title}
+            </h2>
+          </div>
+          <paragraph className="su-big-paragraph">{content}</paragraph>
+        </div>
+        <div className="su-rounded-[8px] su-overflow-hidden">
+          <img src={imageUrl} alt="" className="su-object-cover su-size-full" />
+        </div>
+      </article>
+    </Container>
+  );
 }
