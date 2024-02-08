5035dbeea10825da8ba0595b16e09d8165371eb6
Justin Rent <>
2024-02-08 17:38:09 -0500

    Modified training files for session 3. Will pick back up next week.

---
+++ components/leadership-hero-jr/Component.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+// import specific templates for the component
+import { Container } from "../../packages/grids/Container";
+
+/**
+ * LeadershipHeroJR
+ *
+ * Rendered client side only.
+ */
+
+export default function LeadershipHeroJR(props) {
+  const {title, pubDateFormatted, topic } = props;
+  return (
+    <Container>
+      <div className="su-flex su-justify-between su-flex-wrap su-rs-mt-6">
+        <span className="su-flex su-items-center su-justify-center su-text-[16px] md:su-basefont-23">
+          <time className="su-m-0 su-mr-[4px] su-font-semibold">
+            {pubDateFormatted}
+          </time>
+        </span>
+        <span className="su-ml-auto su-font-semibold su-text-digital-red dark:su-text-dark-mode-red su-text-[16px] sm:su-text-[21px] md:su-text-[24px]">
+          {topic.asset_name}
+        </span>
+      </div>
+      <h1 className="su-mt-[32px] sm:su-mt-[45px] xl:su-mt-[58px] su-font-serif">
+        {title}
+      </h1>
+</Container>
+
+  );
+}
---
+++ components/leadership-hero-jr/server.jsx
@@ -0,0 +1,4 @@
+// This component is gerenated client side only, see client.jsx.
+export default async (args) => {
+  // return renderComponent({ Component, componentName: "leadership-hero-jr", args });
+};
