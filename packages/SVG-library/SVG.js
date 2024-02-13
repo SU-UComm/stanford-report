8a9f63ad3832ab6b90ef5e87fd72f544e79451d4
Isaac Astley <iastley@squiz.net>
2024-02-13 11:41:58 +1100

    adding video autoplay foundations WIP

--- packages/SVG-library/SVG.js
+++ packages/SVG-library/SVG.js
@@ -60,3 +60,5 @@ export { default as Close } from "./Close";
 export { default as BullseyePointer } from "./BullseyePointer";
 export { default as PieChart } from "./PieChart";
 export { default as BarGraph } from "./BarGraph";
+export { default as Pause } from "./Pause";
+export { default as Play } from "./Play";
