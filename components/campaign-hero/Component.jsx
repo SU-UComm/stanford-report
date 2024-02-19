28e818839f75537974e5ca09291dc2c91d672f3c
Justin Rent <>
2024-02-19 17:42:27 -0500

    - Add field groupings and logic to manifest.json - Update preview.data.json with new data structure - Update component.jsx with new data structure

--- components/campaign-hero/Component.jsx
+++ components/campaign-hero/Component.jsx
@@ -16,17 +16,7 @@ import { Container } from "../../packages/grids/Container";
  * @constructor
  */
 
-export default function CampaignHero({
-  bkgImage,
-  title,
-  intro,
-  quote,
-  name,
-  extra,
-  quoteLink,
-  videoUrl,
-  bkgVideo,
-}) {
+export default function CampaignHero({ bkgConfig, textConfig, quoteConfig }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleClick = () => {
@@ -41,17 +31,17 @@ export default function CampaignHero({
       <section>
         <div className="su-sticky su-h-[100vh] su-top-0">
           {/* Background video */}
-          {bkgVideo ? (
+          {bkgConfig.bkgVideo ? (
             <div className="video-container su-relative su-w-full su-h-[100vh] su-overflow-hidden">
               <iframe
-                src={bkgVideo}
+                src={bkgConfig.bkgVideo}
                 title="video"
                 className="su-absolute su-box-border su-min-w-full su-min-h-full su-w-[177.77777778vh] su-h-[56.25vw] su-top-1/2 su-left-1/2 -su-translate-x-1/2 -su-translate-y-1/2"
               />
             </div>
           ) : (
             <img
-              src={bkgImage}
+              src={bkgConfig.bkgImage}
               // src={bgImageData.url}
               className="su-object-cover su-size-full"
               alt="placeholder for local dev"
@@ -65,13 +55,17 @@ export default function CampaignHero({
         </div>
 
         <div className="su-cc su-grid su-grid-cols-6 md:su-grid-cols-12 su-grid-gap su-relative su-z-[2] -su-mt-500">
-          {quote ? (
+          {quoteConfig.quote ? (
             <div className="su-col-span-6 md:su-col-start-1 md:su-col-span-12 lg:su-col-start-3 lg:su-col-span-8 su-text-white su-text-center">
-              <h1 className="su-type-6">{title}</h1>
-              <p className={cnb(quote ? "su-type-3 su-leading-[1.3]" : "")}>
-                {intro}
+              <h1 className="su-type-6">{textConfig.title}</h1>
+              <p
+                className={cnb(
+                  quoteConfig.quote ? "su-type-3 su-leading-[1.3]" : ""
+                )}
+              >
+                {textConfig.intro}
               </p>
-              {videoUrl && (
+              {quoteConfig.videoUrl && (
                 <span className="su-inline-block hocus:su-animate-pulse hocus:su-scale-110 su-transition-all">
                   <button
                     className="su-component-card-thumbnail su-block su-relative su-z-10 su-w-full su-h-full"
@@ -88,7 +82,7 @@ export default function CampaignHero({
                       title="Modal"
                       onClose={handleCloseModal}
                     >
-                      <EmbedVideo videoId={videoUrl} />
+                      <EmbedVideo videoId={quoteConfig.videoUrl} />
                     </Modal>
                   )}
                 </span>
@@ -97,29 +91,31 @@ export default function CampaignHero({
           ) : (
             <>
               <h1 className="su-type-6 su-text-white su-text-center su-mb-450 su-col-span-6 md:su-col-start-1 md:su-col-span-12 lg:su-col-start-3 lg:su-col-span-8 ">
-                {title}
+                {textConfig.title}
               </h1>
               <p className="su-type-3 su-col-span-8 su-text-white su-font-serif su-border-l su-border-color su-border-black-30 su-pl-200 su-py-38 su-mb-0 su-ml-80">
-                {intro}
+                {textConfig.intro}
               </p>
             </>
           )}
-          {quote && (
+          {quoteConfig.quote && (
             <div className="su-rs-pt-2 su-rs-px-5 su-rs-pb-6 su-col-start-2 su-col-span-10 su-text-white su-flex su-items-center su-flex-col su-gap-2xl md:su-flex-row su-border-t su-border-black-30 su-relative su-z-[2]">
               <blockquote cite="" className="su-type-1">
-                <p className="su-font-serif su-leading-display">“{quote}”</p>
+                <p className="su-font-serif su-leading-display">
+                  “{quoteConfig.quote}”
+                </p>
                 <footer className="su-font-bold">
-                  {quoteLink ? (
+                  {quoteConfig.quoteLink ? (
                     <a
-                      href={quoteLink}
+                      href={quoteConfig.quoteLink}
                       className="su-font-bold su-text-white su-decoration-dark-mode-red su-underline-offset-[3px] su-decoration-[0.12em] hocus:su-no-underline hocus:su-text-dark-mode-red"
                     >
-                      {name}
+                      {quoteConfig.name}
                     </a>
                   ) : (
-                    <span>{name}</span>
+                    <span>{quoteConfig.name}</span>
                   )}
-                  {extra && `, ${extra}`}
+                  {quoteConfig.extra && `, ${quoteConfig.extra}`}
                 </footer>
               </blockquote>
               <div className="su-rounded-full su-w-[300px] su-h-[300px] su-grow su-shrink-0 su-basis-auto su-overflow-hidden">
