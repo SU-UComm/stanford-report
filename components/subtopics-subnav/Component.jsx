c0aa706097046078417d81086475ba96c9c1540b
Hannah Malcolm <hmalcolm@squiz.net>
2024-02-20 08:28:58 +1000

    Adjust styles and add container

--- components/subtopics-subnav/Component.jsx
+++ components/subtopics-subnav/Component.jsx
@@ -4,20 +4,18 @@ import { Container } from "../../packages/grids/Container";
 
 function Subnav({ navigation }) {
   let liClass =
-    "su-relative after:su-h-[22px] after:su-w-[1px] after:su-bg-black-60 after:su-absolute after:su-right-[-12px] lg:after:su-right-[-18px] after:su-top-[3px]";
-  let aClass =
-    "su-text-inherit su-text-black su-font-semi-bold su-text-[16px] lg:su-text-[18px] lg:su-leading-[21.6px] su-no-underline hover:su-underline dark:su-text-white";
+    "scrollable-list__item su-mb-0 su-relative after:su-h-[22px] after:su-w-[1px] after:su-bg-black-60 after:su-mx-[12px] lg:after:su-mx-[18px]";
+  const aClass =
+    "su-text-inherit su-text-black su-font-semi-bold su-text-[16px] lg:su-text-[18px] lg:su-leading-[21.6px] su-no-underline hover:su-underline dark:su-text-white hover:su-text-digital-red dark:hover:su-text-dark-mode-red";
   return (
     <div className="scrollable-list su-w-[calc(100%+20px)] md:su-justify-center su-flex su-nowrap su-mb-[15px] md:su-mb-[25px] lg:su-mb-[41px]">
-      <ul className="scrollable-list__items su-w-[calc(100%+40px)] md:su-w-auto su-flex md:su-justify-center su-mb-0 su-whitespace-nowrap su-flex-nowrap md:su-flex-wrap su-overflow-x-scroll md:su-overflow-visible su-list-none su-gap-[24px] lg:su-gap-[36px] su-mx-[-20px] md:su-mr-0 su-px-[20px] su-mb-0">
+      <ul className="scrollable-list__items su-w-[calc(100%+40px)] md:su-w-auto su-flex md:su-justify-center su-mb-0 su-whitespace-nowrap su-flex-nowrap md:su-flex-wrap su-overflow-x-scroll md:su-overflow-visible su-list-none su-mx-[-20px] md:su-mr-0 su-px-[20px] su-mb-0 su-pb-[12px] lg:su-pb-0">
         {navigation.map((item, i, row) => {
           const title = decode(item.asset_name);
 
           if (i + 1 === row.length) {
             // Last one.
-            liClass = "su-relative";
-            aClass =
-              "su-text-inherit su-font-semi-bold su-text-[14px] lg:su-text-[18px] lg:su-leading-[21.6px] su-no-underline hover:su-underline dark:su-text-white";
+            liClass = "su-relative su-mb-0";
           }
 
           return (
@@ -48,14 +46,14 @@ export default function SubtopicSubnav({
   isTopLevel,
 }) {
   return (
-    <Container>
+    <Container width="large">
       <section className="listing">
         <div className="listing__header">
           {!isTopLevel && (
             <p className="listing__back-link su-flex su-justify-center su-mb-[15px] md:su-mb-[18px] lg:su-mb-[10px]">
               <a
                 href={parent.url}
-                className="su-flex su-font-semi-bold su-items-center su-no-underline su-text-[16px] lg:su-text-[18px] su-text-black-60 dark:hover:su-text-dark-mode-red su-ml-[-.5rem] lg:su-ml-[-2rem]"
+                className="su-flex su-font-semi-bold su-items-center su-no-underline su-text-[16px] lg:su-text-[18px] su-text-black-60 hover:su-underline hover:su-text-digital-red dark:hover:su-text-dark-mode-red su-ml-[-.5rem] lg:su-ml-[-2rem]"
               >
                 <svg
                   width="24"
@@ -75,7 +73,7 @@ export default function SubtopicSubnav({
             </p>
           )}
           {title && (
-            <h1 className="su-text-black su-font-bold su-font-serif dark:su-text-white su-text-center su-text-[39px] md:su-text-[54px] lg:su-text-[70px] su-mb-[15px] md:su-mb-[26px] lg:su-mb-[41px]">
+            <h1 className="su-text-black su-font-bold su-font-serif dark:su-text-white su-text-center su-text-[39px] md:su-text-[54px] lg:su-text-[70px] su-mb-[15px] md:su-mb-[26px] lg:su-mb-[19px] su-leading-[46.57px] md:su-leading-[64.8px] lg:su-leading-[84px]">
               {decode(title)}
             </h1>
           )}
