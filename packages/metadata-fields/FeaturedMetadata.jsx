e47c8f3bd0c2b31d6027fe649576b5841b958e40
Isaac Astley <iastley@squiz.net>
2024-02-29 21:33:18 +1100

    updating the metadata fields component based on UAT feedback

--- packages/metadata-fields/FeaturedMetadata.jsx
+++ packages/metadata-fields/FeaturedMetadata.jsx
@@ -218,14 +218,14 @@ export default function FeaturedMetadata({ data }) {
                   className={`md:su-grid ${
                     media >= 3
                       ? gridSettings
-                      : "su-flex su-gap-[10px] su-flex-col md:su-gap-[20px]"
+                      : "su-flex su-gap-[15px] su-flex-col md:su-gap-[25px]"
                   }`}
                 >
                   {media.map(
                     (item) =>
                       item && (
                         <div
-                          className="su-m-0 su-text-[16px] su-leading-[19.1px] su-font-normal su-flex su-flex-col su-gap-[10px] md:su-text-[21px] md:su-leading-[26.25px]"
+                          className="su-m-0 su-text-[16px] su-leading-[19.1px] su-font-normal su-flex su-flex-col su-gap-[5px] md:su-gap-[10px] lg:su-gap-[13px] md:su-text-[21px] md:su-leading-[26.25px]"
                           key={item.asset_assetid}
                         >
                           {item.asset_name}
@@ -235,7 +235,7 @@ export default function FeaturedMetadata({ data }) {
                                 <br />
                                 <a
                                   href={`mailto:${item.asset_metadata_personEmail}`}
-                                  className="su-m-0 su-text-[16px] su-leading-[19.1px] su-font-semibold su-mt-[13px] md:su-text-[21px] md:su-leading-[23.75px]"
+                                  className=""
                                 >
                                   {item.asset_metadata_personEmail}
                                 </a>
@@ -254,8 +254,8 @@ export default function FeaturedMetadata({ data }) {
 
         {firstRow.length === 0 ? (
           <hr
-            aria-hidden="true"
-            className="su-border-none su-flex-grow su-w-[70px] su-h-[2px] su-bg-gradient-to-r su-from-digital-red su-to-plum dark:su-from-palo-verde dark:su-to-olive md:su-w-auto md:su-h-[3px] su-mt-[38px] md:su-mt-0"
+            className="su-bord
+           er-none su-flex-grow su-w-[70px] su-h-[2px] su-bg-gradient-to-r su-from-digital-red su-to-plum dark:su-from-palo-verde dark:su-to-olive md:su-w-auto md:su-h-[3px] su-mt-[38px] md:su-mt-0"
           />
         ) : (
           ""
