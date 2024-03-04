88d35965c63800450b2a435b62e54dd562358503
Isaac Astley <iastley@squiz.net>
2024-03-05 08:12:55 +1100

    updating the metadata fields component as per UAT feedback

--- packages/metadata-fields/FeaturedField.jsx
+++ packages/metadata-fields/FeaturedField.jsx
@@ -8,7 +8,7 @@ export default function FeaturedField({ title, children, alignment = "left" }) {
 
   return (
     <div className={`${alignMap.get(alignment)}`}>
-      <h3 className="su-text-15 su-leading-display su-font-bold su-font-sans su-m-0 su-pb-9 md:su-text-19">
+      <h3 className="su-metadata-fields-title su-text-15 su-leading-display su-font-bold su-font-sans !su-m-0 su-pb-8 md:su-pb-9 md:su-text-19">
         {title}
       </h3>
 
