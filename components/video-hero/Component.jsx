dd543998387b74470afa172cd96d598f8db11c71
Justin Rent <>
2024-03-07 17:03:57 -0500

    - Adds necessary markup to `video-hero` Component.jsx - Adds hydration data for preview to frontend.html preview file - Updates EmbedVideo.jsx file to add className prop for adding classes to the embedded iframe when imported to other components

--- components/video-hero/Component.jsx
+++ components/video-hero/Component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 // import specific templates for the component
 import { Container } from "../../packages/grids/Container";
+import EmbedVideo from "../../packages/media/EmbedVideo";
 
 /**
  * Basic hero
@@ -13,12 +14,33 @@ import { Container } from "../../packages/grids/Container";
  */
 
 export default function VideoHero(props) {
-  const { title } = props;
+  const { title, pubDateFormatted, topic, video } = props;
   return (
     <Container>
-      <div className="su-flex su-justify-between su-flex-wrap su-rs-mt-6 su-rs-mb-5">
-        <h1 className="su-font-serif su-mb-0">{title}</h1>
+      <div className="su-relative su-pb-[56.25%]">
+        <EmbedVideo
+          videoId="kvGRk87kCRE"
+          className="su-absolute su-top-0 su-left-0 su-w-full su-h-full"
+        />
       </div>
+      <div className="su-flex su-justify-between su-flex-wrap su-rs-mt-4">
+        <span className="su-flex su-items-center su-justify-center su-text-16 md:su-basefont-23">
+          <time className="su-m-0 su-mr-4 su-font-semibold">
+            {pubDateFormatted}
+          </time>
+        </span>
+        {topic.asset_name && (
+          <a
+            href={topic.asset_url !== "" ? topic.asset_url : "#"}
+            className="su-font-semibold su-text-digital-red dark:su-text-dark-mode-red su-no-underline hocus:su-underline"
+          >
+            {topic.asset_name}
+          </a>
+        )}
+      </div>
+      <h1 className="su-mt-32 sm:su-mt-45 xl:su-mt-58 su-font-serif">
+        {title}
+      </h1>
     </Container>
   );
 }
