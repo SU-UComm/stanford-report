30306d1dcd8dcbb5b98a9572a9c194fdb47ad538
Justin Rent <>
2024-03-08 16:39:17 -0500

    Adds stub of `video-recommendations` component for carousel at bottom of Video story pages.

---
+++ components/video-recommendations/Component.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+// import specific templates for the component
+import hash from "object-hash";
+import { Container } from "../../packages/grids/Container";
+import { LinkedHeading } from "../../packages/headings/Heading";
+import { Carousel } from "../../packages/carousels/Carousel";
+import Card from "../../packages/card/Card";
+
+/**
+ * Video Recommendations
+ *
+ * @param {string} data The data for rendering the component
+ * @param {string} headingData Title, CTA link elements for the heading
+ * ... any other options needed
+ * @returns {JSX.Element}
+ * @constructor
+ */
+
+export default function VideoRecommendations({ data, headingData }) {
+  const cards = [];
+  const uniqueClass = hash.MD5(
+    JSON.stringify(data) + hash.MD5(JSON.stringify(headingData))
+  );
+
+  if (data !== null && data !== undefined) {
+    data.forEach((card) => {
+      cards.push(<Card cardType="avatar" data={card} />);
+    });
+  }
+
+  return (
+    cards.length > 0 && (
+      <Container>
+        {headingData && (
+          <LinkedHeading
+            title={headingData.title}
+            ctaText={headingData.ctaText}
+            ctaLink={headingData.ctaLink}
+            ctaNewWindow={headingData.ctaNewWindow}
+          />
+        )}
+        <Carousel variant="cards" slides={cards} uniqueClass={uniqueClass} />
+      </Container>
+    )
+  );
+}
---
+++ components/video-recommendations/server.jsx
@@ -0,0 +1,48 @@
+import renderComponent from "../../packages/utils/render-component";
+import Component from "./Component";
+import CardDataAdapter from "../../packages/utils/CardDataAdapter";
+import FunnelbackCardService from "../../packages/utils/FunnelbackCardService";
+import linkedHeadingService from "../../packages/utils/linkedHeadingService";
+
+export default async (args, info) => {
+  // eslint-disable-next-line no-unused-vars
+  const { FB_JSON_URL, API_IDENTIFIER } = info.set.environment;
+  const { ctx } = info;
+  const adapter = new CardDataAdapter();
+  let data = null;
+
+  // check what data source "Search" or "Select"
+  // eslint-disable-next-line no-empty
+
+  if (
+    args.contentConfiguration.searchQuery !== "" &&
+    args.contentConfiguration.searchQuery !== null &&
+    args.contentConfiguration.searchQuery !== "?"
+  ) {
+    // compose and fetch the FB search results
+    const query = args.contentConfiguration.searchQuery;
+    const service = new FunnelbackCardService({ FB_JSON_URL, query });
+
+    adapter.setCardService(service);
+
+    data = await adapter.getCards();
+  }
+
+  // Resolve the URI for the section heading link
+  const headingData = await linkedHeadingService(
+    ctx,
+    args.headingConfiguration
+  );
+
+  const renderProps = {
+    ...args,
+    data,
+    headingData,
+  };
+
+  return renderComponent({
+    Component,
+    componentName: "video-recommendations",
+    args: renderProps,
+  });
+};
