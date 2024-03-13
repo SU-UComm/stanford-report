5b023d11ebc13abd89bd6ff04bde5b1f841dce64
Yvonne Tang <ytang2@stanford.edu>
2024-03-13 13:02:27 -0700

    Button row initial commit

---
+++ components/button-row/Component.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+// import specific templates for the component
+import { Container } from "../../packages/grids/Container";
+
+/**
+ * Button row
+ *
+ * @param {string} title ButtonRow
+ * ... any other options needed
+ * @returns {JSX.Element}
+ * @constructor
+ */
+
+export default function ButtonRow({ title }) {
+  return (
+    <Container>
+      <div />
+    </Container>
+  );
+}
