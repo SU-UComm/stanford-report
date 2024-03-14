fe4c0a26cd0cc82858db26e12c1b4c0d65638ea8
Isaac Astley <iastley@squiz.net>
2024-03-14 14:46:38 +1100

    Adding a headshot image override to the in the news component

--- components/in-the-news/server.jsx
+++ components/in-the-news/server.jsx
@@ -3,11 +3,12 @@ import Component from "./Component";
 import MatrixCardService from "../../packages/utils/MatrixCardService";
 import CardDataAdapter from "../../packages/utils/CardDataAdapter";
 import linkedHeadingService from "../../packages/utils/linkedHeadingService";
+import basicAssetUri from "../../packages/utils/basicAssetUri";
 
 export default async (args, info) => {
   const { featuredContent, supplementaryTeaserOne, supplementaryTeaserTwo } =
     args;
-  const { featuredTeaser } = featuredContent;
+  const { featuredTeaser, personHeadshot } = featuredContent;
   const teaserOne = supplementaryTeaserOne?.teaserOne;
   const teaserTwo = supplementaryTeaserTwo?.teaserTwo;
   const { API_IDENTIFIER } = info.set.environment;
@@ -15,9 +16,15 @@ export default async (args, info) => {
   const adapter = new CardDataAdapter();
   const cards = [];
 
+  let headshotData = null;
   let data = null;
 
   const service = new MatrixCardService({ ctx, API_IDENTIFIER });
+
+  if (personHeadshot) {
+    headshotData = await basicAssetUri(ctx, personHeadshot);
+  }
+
   if (featuredTeaser) {
     cards.push({ cardAsset: featuredTeaser });
   }
@@ -45,6 +52,7 @@ export default async (args, info) => {
     ...args,
     data,
     headingData,
+    headshotData,
   };
 
   return renderComponent({
