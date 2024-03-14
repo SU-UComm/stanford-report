ade0244b8a9cda76eaa1d8699d82a0a9f89e42fd
Yvonne Tang <ytang2@stanford.edu>
2024-03-14 15:13:56 -0700

    row styling; fixup manifest nesting

--- components/button-row/Component.jsx
+++ components/button-row/Component.jsx
@@ -16,19 +16,21 @@ import { LinkButton } from "../../packages/links/LinkButton";
 export default function ButtonRow({ buttonsArray }) {
   return (
     <Container>
-      {buttonsArray.map((button) => {
-        const { buttonText, internalLinkUrl, externalUrl, isNewWindow } =
-          button;
-        return (
-          <LinkButton
-            key={buttonText}
-            buttonText={buttonText}
-            internalUrl={internalLinkUrl}
-            externalUrl={externalUrl}
-            isNewWindow={isNewWindow}
-          />
-        );
-      })}
+      <div className="su-flex su-flex-wrap su-justify-center su-gap-x-18 md:su-gap-x-27 su-gap-y-12 su-mx-auto su-w-fit">
+        {buttonsArray.map((button) => {
+          const { buttonText, internalLinkUrl, externalUrl, isNewWindow } =
+            button;
+          return (
+            <LinkButton
+              key={buttonText}
+              buttonText={buttonText}
+              internalUrl={internalLinkUrl}
+              externalUrl={externalUrl}
+              isNewWindow={isNewWindow}
+            />
+          );
+        })}
+      </div>
     </Container>
   );
 }
