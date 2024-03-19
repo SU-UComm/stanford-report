b1d879916a7fbc1c1c4429a3548066336c85add1
David Cousins <dcousins@squiz.net>
2024-03-19 15:46:31 +1000

    Global CSS updates

---
+++ build/buildTailwind.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-restricted-syntax */
+import fs from "fs";
+import { glob } from "glob";
+import { exec } from "child_process";
+import util from "util";
+
+export async function buildTailwind() {
+  const execPromise = util.promisify(exec);
+  // the global css input
+  const globalInput = "./global/css/_global.css";
+  // final tailwind output
+  const buildPath = "./global/build/";
+  const globalOutput = `${buildPath}global.css`;
+  // final component output
+  const componentOutput = `${buildPath}component.css`;
+
+  await execPromise(`npx tailwind -i ${globalInput} -o ${globalOutput}`);
+
+  // now find all main main.css component files
+  const mainCSS = await glob("./components/*/main.css");
+  // Combine
+  let combinedContent = "";
+  mainCSS.forEach((file) => {
+    combinedContent += `${fs.readFileSync(file, "utf8")}\n`;
+  });
+
+  fs.writeFile(componentOutput, combinedContent, function (err) {
+    if (err) throw err;
+  });
+  await execPromise(`npx tailwind -i ${componentOutput} -o ${componentOutput}`);
+  console.log("componentOutput written");
+}
