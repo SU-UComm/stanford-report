34cfc2032a6c3059e9230e018e2489a8cb8c8de8
Justin Rent <>
2024-03-21 14:41:36 -0400

    - Removes hard coded placeholder image URL from image variation - Moves background-color to image instead of on the container so the caption doesn't have a background.

--- components/basic-story-hero/Component.jsx
+++ components/basic-story-hero/Component.jsx
@@ -75,11 +75,7 @@ export default function basicStoryHero(props) {
         </div>
 
         <div className="su-col-span-6 su-col-start-1 md:su-col-span-12 md:su-col-start-1 su-w-full swiper basic-story__header-slider su-overflow-visible su-rs-mt-4">
-          <figure
-            className={`basic-story__header-image su-col-span-full su-relative su-z-0 ${
-              mediaType === "image" ? "dark:su-bg-black" : ""
-            }`}
-          >
+          <figure className="basic-story__header-image su-col-span-full su-relative su-z-0">
             <div className="su-relative su-w-full">
               <Thumbnail
                 url={media.featureImage.url}
@@ -121,10 +117,9 @@ function Thumbnail({ url, alt, video, type, carousel }) {
   if (type === "image") {
     return (
       <img
-        // src={url}
-        src="https://picsum.photos/350/200"
+        src={url}
         alt={alt}
-        className="su-relative su-w-full su-max-w-full"
+        className="su-relative su-w-full su-max-w-full dark:su-bg-black"
       />
     );
   }
