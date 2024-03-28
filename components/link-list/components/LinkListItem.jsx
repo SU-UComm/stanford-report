21046b10cc2b2ccf2c4e5d5945d8a86651382fae
Hannah Malcolm <hmalcolm@squiz.net>
2024-03-28 12:40:37 +1000

    Allow html entities in card titles

--- components/link-list/components/LinkListItem.jsx
+++ components/link-list/components/LinkListItem.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { XssSafeContent } from "@squiz/xaccel-xss-safe-content";
 
 export default function LinkListItem({ title, url }) {
   return (
     <a href={url} className="su-no-underline hocus:su-underline">
-      <h3 className="su-text-16 su-font-bold su-m-0 lg:su-text-24 lg:su-leading-[28.8px]">
-        {title}
-      </h3>
+      <XssSafeContent
+        className="su-text-16 su-font-bold su-m-0 lg:su-text-24 lg:su-leading-[28.8px]"
+        content={title}
+        elementType="h3"
+      />
     </a>
   );
 }
