fda1039c31e8f218bb71fe73c16802e70bb8f5d8
Isaac Astley <iastley@squiz.net>
2024-04-02 15:06:28 +1100

    updating the announcement links to go to their canonical URL

--- packages/utils/formatCardDataFunnelback.js
+++ packages/utils/formatCardDataFunnelback.js
@@ -10,6 +10,7 @@
 export default function formatCardDataFunnelback({
   title,
   listMetadata: {
+    assetTypeCode,
     teaserPlain,
     summary,
     image,
@@ -28,6 +29,7 @@ export default function formatCardDataFunnelback({
     author,
     isTeaser,
     storySource,
+    canonical,
   },
   date,
   liveUrl,
@@ -60,6 +62,10 @@ export default function formatCardDataFunnelback({
   if (!type && contentCategory) {
     type =
       contentCategory instanceof Array ? contentCategory[0] : contentCategory;
+  }
+
+  if (canonical && assetTypeCode[0] === "link") {
+    liveUrl = canonical;
   }
 
   return {
