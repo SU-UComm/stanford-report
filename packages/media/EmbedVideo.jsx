58c244c5fb2fa57833132ca10d9581ee9dcf0d80
Justin Rent <jrentdesign@gmail.com>
2024-04-03 15:28:25 -0400

    - Adds `noAutoPlay` prop to EmbedVideo package, defaults to autoplay to not break existing imports where it should autoplay (#238)
    - Updates basic-story-hero jsx to noAutoPlay when no thumbnail is added and player is embedded on the page
    - Compiled css + js
    
    Co-authored-by: Justin Rent <>

--- packages/media/EmbedVideo.jsx
+++ packages/media/EmbedVideo.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 
 export default function EmbedVideo(props) {
-  const { videoId, className } = props;
+  const { videoId, className, noAutoPlay } = props;
+
   return (
     <iframe
       width="560"
       height="315"
       className={className}
-      src={`https://www.youtube.com/embed/${videoId}?si=vYU81uVmaV7GSju2&amp;autoplay=1&amp;controls=1&amp;rel=0`}
+      src={`https://www.youtube.com/embed/${videoId}?si=vYU81uVmaV7GSju2&amp;autoplay=${
+        noAutoPlay ? 0 : 1
+      }&amp;controls=1&amp;rel=0`}
       title="YouTube video player"
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
