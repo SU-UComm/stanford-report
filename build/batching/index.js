add9c424254fab0fab176c83720d5b0445519009
David Cousins <dcousins@squiz.net>
2024-04-18 01:06:59 +1000

    topic batching

--- build/batching/index.js
+++ build/batching/index.js
@@ -1,11 +1,14 @@
-import { fetchTopicData, filterData, preparedData, writeTopicBackup, patchTopicData } from "./operations.js";
+import { fetchTopicData, filterData, prepareData, writeTopicBackup, patchTopicData } from "./operations.js";
+import dotenv from 'dotenv';
+dotenv.config();
 
 (async () => {
   const MGT_API = "https://sug-web.matrix.squiz.cloud/__management_api/v1/assets/";
-  const REQUEST_PROPS_GET = {
+  const REQUEST_PROPS = {
     method: "GET",
     headers: {
-      Authorization: "Bearer 653d5ade6d4f19fa7dfc885d7a1e6cc9",
+      'Authorization': `Bearer ${process.env.MANAGEMENT_API_KEY}`,
+      'Content-Type': 'application/json'
     },
   }
   //
@@ -15,47 +18,64 @@ import { fetchTopicData, filterData, preparedData, writeTopicBackup, patchTopicD
   // the batching requires an array of IDs
   // the topicData objects contain a target_id that are the IDs we need (they are the pages we want to update)
   const topicData = await fetchTopicData();
-  let filteredTopicData = await filterData(topicData);
-  filteredTopicData = filteredTopicData.slice(0, 10);
-  // console.log(filteredTopicData);
+   // type = ["content_type", "featured_unit", "content_topic"]
+  let filteredTopicData = await filterData(topicData, "featured_unit");
+  // filteredTopicData = filteredTopicData.slice(0, 10);
+  filteredTopicData = [
+    { target_id: "142706" },
+    { target_id: "134217" },
+    { target_id: "134236" },
+    { target_id: "142945" }
+  ];
 
-  let apiPageData = await preparedData(filteredTopicData, MGT_API, REQUEST_PROPS, true);
+  console.log(filteredTopicData.length + " results");
 
+  // prepare the data that we will eventally send
+  let apiPageData = await prepareData(filteredTopicData, MGT_API, REQUEST_PROPS, true);
+  // good idea to store a backup of the previous version
   await writeTopicBackup(apiPageData);
 
   // now merge the data
+  // if we need to update the component version
+  const COMPONENT = "stanford-components/topic-subtopic-listing";
+  const VERSION = "0.1.0";
+
   apiPageData.forEach(topic => {
-    topic.attributes.dxp_page_content.layouts[0].content.main[0].contentItem.content.displayConfiguration.searchQuery = topic.newQuery;
+    // Target our management API content block
+    const contentBlockTarget = topic.attributes.dxp_page_content.layouts[0].content.main[0];
+    // for topics, set the searchQuery string to the new query
+    contentBlockTarget.contentItem.content.displayConfiguration.searchQuery = topic.newQuery;
+    // and then delete the temp key
     delete topic.newQuery;
-    console.log(topic.attributes.dxp_page_content.layouts[0].content.main[0].contentItem.content.displayConfiguration.searchQuery);
+
+    // set the versioning
+    contentBlockTarget.componentId = `${COMPONENT}/${VERSION}`;
+    contentBlockTarget.contentItem.schemaName = `${COMPONENT}/${VERSION}/main`;
+
+    // console.log(topic.attributes.dxp_page_content.layouts[0].content.main[0].contentItem);
+    // console.log(contentBlockTarget.contentItem.content);
   });
 
-  // const patchData = await patchTopicData(apiPageData, MGT_API, REQUEST_PROPS, true, "PATCH");
-
-
-
-
-
-
-
   
+  //
+  // ###################################
+  // ###################################
+  // ###################################
+  // BEWARE! Read carefully before proceeding
+  // ###################################
+  // ###################################
+  // ###################################
+  //
+  // By running the following command, you will initiate the batch update to the Matrix management API.
+  // It will patch the entire body of a content page to the API, effectively updating the entire asset structure.
+  // so, make sure you have the correct data before enabling this line of code
+  // 
+  // 
+  console.log(`About to update ${apiPageData.length} assets.`);
+  console.log("...");
+  const patchData = await patchTopicData(apiPageData, MGT_API, REQUEST_PROPS);
+  console.log(`Patched ${patchData.length} items`);
 
-  // Are we setting a version or content?
-  const UPDATEVERSION = false;
-  const UPDATECONTENT = true;
-
-  const COMPONENT = "stanford-components/topic-subtopic-listing";
-  const VERSION = "0.0.13";
-
-
-  const searchQuery = `?profile=stanford-report-push-search
-                        &collection=sug~sp-stanford-report-search
-                        &sort=title
-                        {&meta_isTeaser_not=false}
-                        &meta_taxonomyContentCategoryId={topicid}`;
-
-  // the dot notation location of where we need to set the data
-  const datamodel = "attributes?.dxp_page_content?.layouts?[0]?.content?.main?[0]?.contentItem?.content?.displayConfiguration?.searchQuery";
 
 
 
--- build/batching/operations.js
+++ build/batching/operations.js
@@ -9,14 +9,29 @@ export async function fetchTopicData() {
 };
 
 
-export async function filterData(topics) {
- // we need to exclude all top level section pages
-    // All section pages have a group: "28170" value
+export async function filterData(topics, type = null) {
+  // type = ["content_type", "featured_unit", "content_topic"]
+  // we need to exclude all top level section pages
+  // All section pages have a group: "28170" value
   const output = [];
   Object.entries(topics).forEach(([key, value]) => {
-    // if it is not a section page
+    //
+    // MAKE SURE WE DO NOT OVERRIDE SECTION PAGES (the main top level pages)
+    // Each section page has a group value of 28170
+    // 
     if(value.group !== "28170"){
-      output.push(value);
+
+      // if we have passed a type filter in, we only want to return that type
+      if( type ) {
+        if( type && type === value.type ) {
+          output.push(value);
+        }
+      } else {
+        output.push(value);
+      }
+      
+
+
     }
   });
 
@@ -25,60 +40,70 @@ export async function filterData(topics) {
 
 export async function writeTopicBackup(jsonData) {
   const data = JSON.stringify(jsonData);
-  fs.writeFile('./build/batching/backups/topic-data-backup.json', data, (err) => {
+  const timestamp = Date.now();
+  fs.writeFile(`./build/batching/backups/${timestamp}-topic-data-backup.json`, data, (err) => {
     if (err) throw err;
-    console.log('Data has been saved to ./build/batching/backups/topic-data-backup.json');
+    console.log(`Backup data has been saved to ./build/batching/backups/${timestamp}-topic-data-backup.json`);
     return;
   });
 }
 
 
-export async function preparedData(topicsArr, mgt_api, requestProps, log = true) {
+export async function prepareData(topicsArr, mgt_api, requestProps) {
       // create a promise array
       const promises = topicsArr.map(async (topic) => {
         const url = `${mgt_api}${topic.target_id}`;
-
+        // fetch initial topic page data
         const res = await fetch(url, requestProps).catch((error) => {
           throw new Error(error);
         });
         const json = await res.json();
 
+        // filter out and rework the FB queries
         const query = json.attributes?.dxp_page_content?.layouts[0]?.content?.main[0]?.contentItem?.content?.displayConfiguration?.searchQuery;
         let cleanQuery = query
           .replace("&meta_isTeaser_not=false", "")
           .replace("&sort=title", "")
           .replace("&sort=date", "");
 
+          // other possible filters we may need to add
+          // exclude Announcement #28210
+          //&meta_taxonomyContentTypeId_not=28210
+          // Exclude In the News #28216
+          //&meta_taxonomyContentTypeId_not=28216
+          // exclude Leadership Messages #28201
+          //&meta_taxonomyContentTypeId_not=28201
+
         // add sorting
         cleanQuery += "&sort=date";
+        // add a temp value (it is removed later)
         json.newQuery = cleanQuery;
 
+        // return the modified json
         return json;
       });
 
       // Wait for all promises to resolve before continuing
       return Promise.all(promises);
-  
 }
 
 
-export async function patchTopicData(topicsArr, mgt_api, requestProps, log = true, method = "GET") {
+export async function patchTopicData(topicsArr, mgt_api, requestProps) {
       // create a promise array
-      requestProps.method = method;
+      requestProps.method = "PATCH";
+
       const promises = topicsArr.map(async (topic) => {
-        const url = `${mgt_api}${topic.target_id}`;
+        const url = `${mgt_api}${topic.id}`;
 
-        requestProps.body = JSON.strinfigy(topic);
-
+        requestProps.body = JSON.stringify(topic);
          const res = await fetch(url, requestProps).catch((error) => {
            throw new Error(error);
          });
         const json = await res.json();
-
+        console.log(`${topic.id} written.`);
         return json;
       });
 
       // Wait for all promises to resolve before continuing
       return Promise.all(promises);
-  
 }
