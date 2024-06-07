1f87ecafbc206e7d6ecbb336c3373ca6cac44b75
Isaac Astley <iastley@squiz.net>
2024-06-07 12:11:35 +1000

    fixing the z-index of the stories for you 'link-list' component

--- components/link-list/Component.jsx
+++ components/link-list/Component.jsx
@@ -52,7 +52,7 @@ export default function LinkList({ search }) {
       data-role="link-list-wrapper"
       className={`${
         linkItems.length ? "" : "su-link-list-no-stories"
-      } su-fixed su-opacity-0 su--bottom-[100 su-left-0 su-left-1/2 su-right-1/2 su-translate-x-[-50%] su-max-w-[482px] su-p-20 su-bg-foggy-light lg:dark:su-bg-black-true dark:su-bg-black su-linklist-mob-width su-rounded-tl-[8px] su-rounded-tr-[8px] su-transition su-z-[9999] su-px-30 su-linklist-mob-width lg:su-z-[1] lg:su-bg-white lg:su-relative lg:su-bottom-0 lg:su-opacity-100 lg:su-p-0 lg:su-w-full`}
+      } su-fixed su-opacity-0 su--bottom-[100 su-left-0 su-left-1/2 su-right-1/2 su-translate-x-[-50%] su-max-w-[482px] su-p-20 su-bg-foggy-light lg:dark:su-bg-black-true dark:su-bg-black su-linklist-mob-width su-rounded-tl-[8px] su-rounded-tr-[8px] su-transition su-z-[49] su-px-30 su-linklist-mob-width lg:su-z-[1] lg:su-bg-white lg:su-relative lg:su-bottom-0 lg:su-opacity-100 lg:su-p-0 lg:su-w-full`}
     >
       <div className="su-flex">
         {linkItems.length ? (
