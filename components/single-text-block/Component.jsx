6958db92c050a7793adbee654fa31f77461c8fa6
Yvonne Tang <ytang2@stanford.edu>
2024-07-12 17:48:25 -0700

    field name and styles updates

--- components/single-text-block/Component.jsx
+++ components/single-text-block/Component.jsx
@@ -1,29 +1,28 @@
 import React from "react";
 import { XssSafeContent } from "@squiz/xaccel-xss-safe-content";
-import { Container } from "../../packages/grids/Container";
 
 /**
  * Single Text Block component
  *
- * @param {string} title The title of the text card
- * @param {string} eyebrow The eyebrow/superheading of the text card
- * @param {string} content The content on the flip side of the text card
+ * @param {string} title The title of the text block
+ * @param {string} eyebrow The eyebrow/superheading
+ * @param {string} description The body content
  * @returns {JSX.Element}
  * @constructor
  */
 
-export default function SingleTextBlock({ title, eyebrow, content }) {
+export default function SingleTextBlock({ title, eyebrow, description }) {
   return (
     <div className="su-cc su-rs-my-10">
       <div className="su-ml-0 su-max-w-[110rem] su-border-l-2 su-border-black-30 dark:su-border-black-60 su-rs-py-3 su-pl-38 md:su-pl-76 2xl:su-pl-170">
-        <span className="su-inline-block su-text-black-60 dark:su-text-black-30 su-font-semibold su-type-1 su-leading-display su-rs-mb-1">
+        <span className="su-inline-block su-text-black-60 dark:su-text-black-40 su-font-semibold su-type-1 su-leading-display su-rs-mb-1">
           {eyebrow}
         </span>
         <h2 className="su-type-5 su-rs-mb-5 dark:su-text-white">{title}</h2>
         <XssSafeContent
           data-test="single-text-block-content"
           className="su-type-3 *:su-leading-snu *:last:su-mb-0 dark:su-text-white"
-          content={content}
+          content={description}
         />
       </div>
     </div>
