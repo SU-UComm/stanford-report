c8ece71f68a6afe9d23de0df6f76fb892ed1937e
Yvonne Tang <ytang2@stanford.edu>
2024-07-15 09:44:52 -0700

    Initial setup single cta block

---
+++ components/single-cta-block/Component.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+// import specific templates for the component
+import { Container } from "../../packages/grids/Container";
+import { LinkedHeading } from "../../packages/headings/Heading";
+
+/**
+ * Single CTA Block component
+ *
+ * @param {string} Single CTA Block
+ * @returns {JSX.Element}
+ * @constructor
+ */
+
+export default function SingleCtaBlock({ title }) {
+  return (
+    <Container>
+      cdcdcdcd
+      <LinkedHeading title={title} />
+    </Container>
+  );
+}
