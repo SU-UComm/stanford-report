a0016c938f7cddebb66c10b4fd70ddc7d1dfe046
Rajoshi Chakravarty <rchakravarty@SQUIZ-2021-C02DVASXML7H.local>
2024-07-15 18:14:42 -0700

    popular stories cf changes

--- components/popular-stories-cf/server.jsx
+++ components/popular-stories-cf/server.jsx
@@ -21,9 +21,79 @@ function getDateRange(range) {
   // return `${year}-${month}-${day}`;
 }
 
+function formatDate(date) {
+  const day = String(date.getDate()).padStart(2, "0");
+  const monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const month = monthNames[date.getMonth()];
+  const year = date.getFullYear();
+
+  return `${day}${month}${year}`;
+}
+
+function getAPIDateRange(date) {
+  const dateRangeMap = {
+    "1 week": 7,
+    "2 weeks": 14,
+    "1 month": 30,
+    "6 months": 180,
+    "1 year": 365,
+  };
+  return dateRangeMap(date);
+}
+
+function getMaxPublishedRange(date) {
+  const dateRangeMap = {
+    "Past 6 months": 0.5,
+    "Past 1 year": 1,
+    "Past 2 years": 2,
+  };
+  return dateRangeMap(date);
+}
+
 export default async (args, info) => {
   const adapter = new FetchAdapter();
   const { FB_JSON_URL, MGT_API, CF_ANALYTICS_API } = info.set.environment;
+  const {
+    storiesCount,
+    APIrespCount,
+    sourcePath,
+    assetExclusions,
+    contentTypeExclusions,
+    APIdateRange,
+    publishedDateMax,
+  } = args;
+
+  const dateRangeNumeric = getAPIDateRange(APIdateRange);
+  const exclusionContentTypes = contentTypeExclusions.map((num) =>
+    `taxonomyContentTypeId:${num}`.join(" ")
+  );
+
+  const exclusionIDs = assetExclusions.map((num) => `id:${num}`.join(" "));
+
+  const publishedDateRangeNumeric = getMaxPublishedRange(publishedDateMax);
+
+  // Today's date
+  const today = new Date();
+  const todayFormatted = formatDate(today);
+  // Date from input
+  const rangeDate = new Date();
+  rangeDate.setFullYear(today.getFullYear() - publishedDateRangeNumeric);
+  const dateRange = formatDate(rangeDate);
+
+  const dateRangeQuery = `&f.Date%7Cd=d>${dateRange}<${todayFormatted}`;
 
   // 2024-05-01T00:00:00.000Z
 
@@ -33,18 +103,18 @@ export default async (args, info) => {
           zones(filter: { zoneTag_in: ["4f245bec971a45ca38091352b2e11951"] }) {
               httpRequestsAdaptiveGroups(
                   filter: {
-                      datetime_gt: "${getDateRange(7)}"
+                      datetime_gt: "${getDateRange(dateRangeNumeric)}"
                       datetime_lt: "${getDateRange()}"
                       clientRequestHTTPHost: "news.stanford.edu"
                       requestSource: "eyeball"
                       edgeResponseStatus_lt: 300
                       AND: [
-                          { clientRequestPath_like: "/stories/%" }
+                          { clientRequestPath_like: "${sourcePath}%" }
                           { clientRequestPath_notlike: "%/_admin%" }
                       ]
                   }
                   orderBy: [count_DESC]
-                  limit: 10
+                  limit: ${APIrespCount}
               ) {
                   count
                   dimensions {
@@ -79,9 +149,16 @@ export default async (args, info) => {
       }
     );
 
-    data = await popularStoriesFetcher(urls, {
-      FB_JSON_URL,
-    });
+    data = await popularStoriesFetcher(
+      urls,
+      storiesCount,
+      exclusionContentTypes,
+      exclusionIDs,
+      dateRangeQuery,
+      {
+        FB_JSON_URL,
+      }
+    );
   } else {
     data = [];
   }
