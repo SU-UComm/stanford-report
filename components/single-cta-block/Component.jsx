e05626ce119ab5074b579deac59d4f5991e3e441
Yvonne Tang <ytang2@stanford.edu>
2024-07-17 12:20:57 -0700

    Add image

--- components/single-cta-block/Component.jsx
+++ components/single-cta-block/Component.jsx
@@ -5,7 +5,15 @@ import { LinkButton } from "../../packages/links/LinkButton";
 /**
  * Single CTA Block component
  *
- * @param {string} Single CTA Block
+ * @param {string} title
+ * The title of the block
+ *
+ * @param {string} eyebrow
+ * The superheading above the title
+ *
+ * @param {string} description
+ * The content of the block from the WYSIWYG
+ *
  * @returns {JSX.Element}
  * @constructor
  */
@@ -14,6 +22,7 @@ export default function SingleCtaBlock({
   title,
   eyebrow,
   description,
+  imageUrl,
   ctaConfiguration,
   internalLinkUrl,
 }) {
@@ -21,38 +30,47 @@ export default function SingleCtaBlock({
     ctaConfiguration;
   return (
     <div className="su-cc">
-      <div className="su-rounded-[8px] su-rs-py-8 su-rs-px-4 su-break-words su-transition-shadow su-border dark:su-border-2 su-border-black-30/30 dark:su-border-black su-shadow dark:su-shadow-black/80 su-bg-white dark:su-bg-black-true su-text-black dark:su-text-white">
-        <span className="su-block su-mx-auto su-mb-02em su-text-center su-text-black-60 dark:su-text-black-40 su-font-semibold su-type-1 su-leading-display">
-          {eyebrow}
-        </span>
-        <div className="su-mx-auto xl:su-max-w-900 su-flex su-flex-col md:su-flex-row su-gap-20 su-items-center su-rs-mb-4">
-          <div
-            aria-hidden
-            className="su-relative md:su-top-06em su-hidden su-min-w-100 md:su-block su-grow su-shrink-0 su-h-4 su-bg-gradient-to-r su-from-digital-red-light su-to-digital-red-dark dark:su-from-palo-verde dark:su-to-olive su-w-auto"
+      <div className="su-flex su-flex-col-reverse xl:su-flex-row-reverse su-items-center su-justify-center su-gap-34 xl:su-gap-60 su-rounded-[8px] su-rs-py-8 su-rs-px-4 su-break-words su-transition-shadow su-border dark:su-border-2 su-border-black-30/30 dark:su-border-black su-shadow dark:su-shadow-black/80 su-bg-white dark:su-bg-black-true su-text-black dark:su-text-white">
+        <div>
+          <span className="su-block su-mx-auto su-mb-02em su-text-center su-text-black-60 dark:su-text-black-40 su-font-semibold su-type-1 su-leading-display">
+            {eyebrow}
+          </span>
+          <div className="su-mx-auto xl:su-max-w-900 su-flex su-flex-col md:su-flex-row su-gap-20 su-items-center su-rs-mb-4">
+            <div
+              aria-hidden
+              className="su-relative md:su-top-06em su-hidden su-min-w-100 md:su-block su-grow su-shrink-0 su-h-4 su-bg-gradient-to-r su-from-digital-red-light su-to-digital-red-dark dark:su-from-palo-verde dark:su-to-olive su-w-auto"
+            />
+            <h2 className="su-mx-auto su-text-center su-leading-tight su-type-4 dark:su-text-white su-mb-0">
+              {title}
+            </h2>
+            <div
+              aria-hidden
+              className="su-relative md:su-top-06em su-mx-auto su-min-w-100 su-grow su-shrink-0 su-w-180 su-h-4 su-bg-gradient-to-l su-from-digital-red-light su-to-digital-red-dark dark:su-from-palo-verde dark:su-to-olive md:su-w-auto su-mb-20 md:su-mb-0"
+            />
+          </div>
+          <XssSafeContent
+            data-test="single-text-block-content"
+            className="su-mx-auto su-text-center su-max-w-prose-wide su-type-1 *:su-leading-snug *:last:su-mb-0 dark:su-text-white"
+            content={description}
           />
-          <h2 className="su-mx-auto su-text-center su-leading-tight su-type-4 dark:su-text-white su-mb-0">
-            {title}
-          </h2>
-          <div
-            aria-hidden
-            className="su-relative md:su-top-06em su-mx-auto su-min-w-100 su-grow su-shrink-0 su-w-180 su-h-4 su-bg-gradient-to-l su-from-digital-red-light su-to-digital-red-dark dark:su-from-palo-verde dark:su-to-olive md:su-w-auto su-mb-20 md:su-mb-0"
-          />
+          {ctaText && (
+            <LinkButton
+              variant="gradient"
+              size="large"
+              buttonText={ctaText}
+              internalUrl={internalLinkUrl}
+              externalUrl={externalUrl}
+              email={email}
+              isNewWindow={isNewWindow}
+              className="!su-block su-w-fit su-rs-mt-4 su-mx-auto"
+            />
+          )}
         </div>
-        <XssSafeContent
-          data-test="single-text-block-content"
-          className="su-mx-auto su-text-center su-max-w-prose-wide su-type-1 *:su-leading-snug *:last:su-mb-0 dark:su-text-white"
-          content={description}
-        />
-        {ctaText && (
-          <LinkButton
-            variant="gradient"
-            size="large"
-            buttonText={ctaText}
-            internalUrl={internalLinkUrl}
-            externalUrl={externalUrl}
-            email={email}
-            isNewWindow={isNewWindow}
-            className="!su-block su-w-fit su-rs-mt-4 su-mx-auto"
+        {imageUrl && (
+          <img
+            src={imageUrl}
+            alt=""
+            className="su-w-1/2 sm:su-w-1/3 xl:su-w-1/4 su-shrink-0"
           />
         )}
       </div>
--- components/single-cta-block/server.jsx
+++ components/single-cta-block/server.jsx
@@ -1,10 +1,21 @@
 import renderComponent from "../../packages/utils/render-component";
 import Component from "./Component";
+import basicAssetUri from "../../packages/utils/basicAssetUri";
 
-export default async (args) => {
+export default async (args, info) => {
+  const { ctx } = info;
+  let imageData = null;
+  imageData = await basicAssetUri(ctx, args.image);
+  const imageUrl = imageData.url;
+
+  const renderProps = {
+    ...args,
+    imageUrl,
+  };
+
   return renderComponent({
     Component,
     componentName: "single-cta-block",
-    args,
+    args: renderProps,
   });
 };
