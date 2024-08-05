6909b56d3c3238f60d49c2dfffac329ade0cbcc0
Justin Rent <>
2024-08-05 10:42:42 -0400

    - WIP adding external link arrow to veritcalCard

--- components/featured-content/Component.jsx
+++ components/featured-content/Component.jsx
@@ -32,6 +32,8 @@ export default function FeaturedContent({
     featuredCardData.description = contentConfiguration.featuredDescription;
   }
 
+  console.log(data);
+
   return (
     <Container width="large" data-component="featured-content">
       <LinkedHeading
