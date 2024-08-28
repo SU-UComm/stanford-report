cd58db99b84586205c3ed2f9cb95379ffae0f878
Yvonne Tang <ytang2@stanford.edu>
2024-08-27 17:48:38 -0700

    Use flexbox instead of grid to accommodate 1-3 items

--- components/vertical-videos-panel/styles.js
+++ components/vertical-videos-panel/styles.js
@@ -2,7 +2,7 @@ export const root = "su-relative su-overflow-hidden su-bg-black su-break-words";
 export const wrapper = "su-relative su-z-30";
 export const sectionHeading = "2xl:su-px-[17rem] su-rs-mb-5";
 export const cardGrid =
-  "su-relative su-mx-auto su-hidden lg:su-grid lg:su-grid-cols-3 su-gap-20 xl:su-gap-40 su-z-30 su-list-unstyled *:su-mb-0";
+  "su-relative su-mx-auto su-hidden lg:su-flex lg:*:su-basis-1/3 su-justify-center su-gap-20 xl:su-gap-40 su-z-30 su-list-unstyled *:su-mb-0";
 export const bgImage =
   "su-absolute su-size-full su-inset-0 su-object-cover su-inset-0";
 export const overlay =
