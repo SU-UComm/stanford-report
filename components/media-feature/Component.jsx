159f7e4a777120686f4dd00b55e0a4bd8d0436ed
Justin Rent <>
2025-01-16 17:10:49 -0500

    - Add "Magazine" option and appropriate logic to component - Add "Magazine" option to manifest - Adjustments to preview data for testing

--- components/media-feature/Component.jsx
+++ components/media-feature/Component.jsx
@@ -1,13 +1,11 @@
 import React from "react";
+import { cnb } from "cnbuilder";
 
 // import specific templates for the component
 import { Container } from "../../packages/grids/Container";
 import { SidebarHeading } from "../../packages/headings/Heading";
-import {
-  Podcast,
-  ExternalArrow,
-  BookOpenCover,
-} from "../../packages/SVG-library/SVG";
+import { FAIcon } from "../../packages/icons/FAIcon";
+import { ExternalArrow } from "../../packages/SVG-library/SVG";
 
 /**
  * Base component
@@ -25,33 +23,39 @@ export default function MediaFeature({
 }) {
   const { title, teaserText, mediaType, linkUrl } = contentConfiguration;
 
-  const thumbMap = new Map();
+  let typeIcon;
+  let iconTestId;
 
-  thumbMap.set(
-    "Book",
-    "su-shadow-[0px_4px_7px_0px_rgba(0,0,0,0.15)] su-size-full su-object-scale-down su-object-center"
-  );
-  thumbMap.set(
-    "Podcast",
-    "su-absolute su-object-cover su-rounded-[8px] su-z-[2] su-flex-1 su-size-full su-shadow-[0px_4px_7px_0px_rgba(0,0,0,0.15)]"
-  );
+  if (contentConfiguration.mediaType === "Book") {
+    typeIcon = "book-open-cover";
+    iconTestId = "svg-book-open-cover";
+  } else if (contentConfiguration.mediaType === "Podcast") {
+    typeIcon = "microphone";
+    iconTestId = "svg-microphone";
+  } else if (contentConfiguration.mediaType === "Magazine") {
+    typeIcon = "book-open";
+    iconTestId = "svg-book-open";
+  }
 
   return (
     <Container width="full" paddingX={false}>
       <section className="su-py-45 su-px-20 su-flex su-justify-center su-relative md:su-py-72 md:su-px-50">
         <div className="su-group su-max-w-[1086px] su-flex su-flex-col su-items-center su-z-[2] su-relative su-p-38 before:su-content-[''] before:su-bg-foggy-light before:su-w-full before:su-h-full before:su-opacity-90 before:su-absolute before:su-z-[-1] before:su-top-0 before:su-left-0 md:su-flex-row md:su-gap-20 md:su-items-start lg:su-p-48 lg:su-gap-48">
           <div
-            className={`${
+            className={cnb(
+              "su-h-[224px] md:su-h-[182px] lg:su-h-[292px] su-relative su-shrink-0",
               mediaType === "Podcast" &&
-              "su-w-[224px] md:su-w-[182px] lg:su-w-[292px]"
-            } su-h-[224px] md:su-h-[182px] lg:su-h-[292px] su-relative su-shrink-0 ${
-              mediaType === "Book" && "media-feature__thumb"
-            }`}
+                "su-w-[224px] md:su-w-[182px] lg:su-w-[292px]"
+            )}
           >
             <img
               src={imageData.url}
               alt={imageData.attributes.alt}
-              className={thumbMap.get(mediaType)}
+              className={cnb(
+                mediaType === "Podcast"
+                  ? "su-absolute su-object-cover su-rounded-[8px] su-z-[2] su-flex-1 su-size-full su-shadow-[0px_4px_7px_0px_rgba(0,0,0,0.15)]"
+                  : "su-shadow-[0px_4px_7px_0px_rgba(0,0,0,0.15)] su-size-full su-object-scale-down su-object-center"
+              )}
             />
           </div>
 
@@ -73,7 +77,13 @@ export default function MediaFeature({
             </h3>
 
             <div className="su-w-full su-flex su-gap-[0.6rem] su-text-18 su-text-black-70 su-font-semibold su-items-center su-pb-15 su-leading-snug md:su-pb-19 md:su-text-16">
-              <MediaType type={mediaType} />
+              <FAIcon
+                icon={typeIcon}
+                set="solid"
+                data-testid={iconTestId}
+                width={18}
+              />
+              <span className="su-font-semibold">{mediaType}</span>
             </div>
 
             <p className="su-text-18 su-w-full su-m-0 su-leading-[125%] su-text-black su-font-normal md:su-text-19 lg:su-text-21">
@@ -92,24 +102,28 @@ export default function MediaFeature({
   );
 }
 
-function MediaType({ type }) {
-  const iconMap = new Map();
+// function MediaType({ type }) {
+//   const iconMap = new Map();
 
-  iconMap.set("Podcast", <Podcast variant="outline" />);
-  iconMap.set("Book", <BookOpenCover className="su-w-[1.2em]" aria-hidden />);
+//   iconMap.set("Podcast", <Podcast variant="outline" />);
+//   iconMap.set("Book", <BookOpenCover className="su-w-[1.2em]" aria-hidden />);
 
-  return (
-    <>
-      <span className="*:su-size-20">{iconMap.get(type)}</span>
-      {type}
-    </>
-  );
-}
+//   return (
+//     <>
+//       <span className="*:su-size-20">{iconMap.get(type)}</span>
+//       {type}
+//     </>
+//   );
+// }
 
 function FeaturedHeading({ type }) {
   switch (type) {
     case "Podcast":
       return <SidebarHeading icon="Featured audio" title="Featured audio" />;
+    case "Magazine":
+      return (
+        <SidebarHeading icon="Featured reading" title="Featured reading" />
+      );
     default:
       return <SidebarHeading icon="Featured reading" title="Featured book" />;
   }
