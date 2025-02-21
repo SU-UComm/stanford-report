25c7253f3673baca8b2e3926980983b6906cce2f
Justin Rent <>
2025-02-20 21:07:05 -0500

    - Add boolean to hide/show bottom border and adjust styles - Cleanup

--- components/multicolumn-info-section/Component.jsx
+++ components/multicolumn-info-section/Component.jsx
@@ -2,41 +2,32 @@ import React from "react";
 import { cnb } from "cnbuilder";
 import { XssSafeContent } from "@squiz/xaccel-xss-safe-content";
 import { Container } from "../../packages/grids/Container";
-import TextCallout from "../text-callout/Component";
 import InfoBox from "../../packages/info-box/InfoBox";
 
 /**
  * Multicolumn Info Section component
- *
- * @param {string} title
- * The title of the text block.
- *
- * @param {string} eyebrow
- * The eyebrow/superheading.
- *
- * @param {string} paddingY
- * Responsive vertical padding of the container
- *
- * @param {array} facts
- * A array of 1 to 3 facts each with a FontAwesome icon and a text description.
  *
  * @returns {JSX.Element}
  * @constructor
  */
 
 export default function MulticolumnInfoSection({
+  border,
   colOne,
   colTwo,
   colThree,
   imageData,
   internalLinkUrl,
 }) {
-  console.log(imageData);
-  console.log(internalLinkUrl);
-  console.log(internalLinkUrl);
   return (
     <Container width="wide">
-      <div className="su-grid su-grid-cols-6 su-grid-gap su-gap-y-0 md:su-grid-cols-12 su-border-b su-rs-pb-5">
+      <div
+        className={cnb(
+          "su-grid su-grid-cols-6 su-grid-gap su-gap-y-0 md:su-grid-cols-12",
+          border &&
+            "su-border-b su-rs-pb-5 su-border-b-black-30 dark:su-border-b-black"
+        )}
+      >
         <h2 className="su-type-2 su-col-span-full md:su-col-span-4 lg:su-col-span-3">
           {colOne.title}
         </h2>
