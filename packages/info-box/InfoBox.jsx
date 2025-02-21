63147e37c4fb37db15d006d2c6f04208eb99ef39
Justin Rent <>
2025-02-21 14:54:47 -0500

    - Adjust heading size in InfoBox. - Adjust spacing in InfoBox. - Add max-width styles for TextCallout on non-story pages. - Adjust spacing of paragraphs in MulticolumnInfoSection

--- packages/info-box/InfoBox.jsx
+++ packages/info-box/InfoBox.jsx
@@ -68,10 +68,10 @@ export default function InfoBox({
 
   return (
     <Container width="full" paddingX={false} className={containerClassName}>
-      <section className="su-flex su-flex-col su-p-20 md:su-p-36 su-justify-start su-items-start su-bg-fog-light lg:su-mx-auto dark:su-bg-black [&>p]:su-m-0 [&>p]:!su-mb-0 [&>p]:su-text-16 md:[&>p]:!su-text-19 last-of-type:[&>p]:!su-mb-0">
-        <div className="su-relative su-justify-start su-items-center su-w-full su-gap-3 su-flex su-overflow-hidden su-mb-12">
+      <div className="su-flex su-flex-col su-p-20 md:su-p-36 su-justify-start su-items-start su-bg-fog-light lg:su-mx-auto dark:su-bg-black [&>p]:su-m-0 [&>p]:!su-mb-0 [&>p]:su-text-16 md:[&>p]:!su-text-19 last-of-type:[&>p]:!su-mb-0">
+        <div className="su-relative su-justify-start su-items-center su-w-full su-gap-3 su-flex su-overflow-hidden su-rs-mb-0">
           <div>
-            <h3 className="su-font-serif su-inline !su-text-23 su-pr-10 su-m-0">
+            <h3 className="su-font-serif su-text-21 md:su-text-23 su-inline su-pr-10 su-m-0">
               {title}
             </h3>
             <span className="su-w-full su-bg-black-40 dark:su-bg-black-70 su-h-px su-absolute su-bottom-4" />
@@ -126,7 +126,7 @@ export default function InfoBox({
             className="su-rs-mt-1 su-order-3"
           />
         )}
-      </section>
+      </div>
     </Container>
   );
 }
