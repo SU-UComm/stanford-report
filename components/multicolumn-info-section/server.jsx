c2e38b9ac1213a6f7360ebe1c4e20ce0a93ddfc7
Justin Rent <jrentdesign@gmail.com>
2025-03-11 09:49:09 -0400

    UCP-3976  Add CTA button to multicolumn info section (#349)
    * - Update component to add button
    - Add new preview data file
    - Increment for dev testing deploy
    
    * - Adjust manifest.json logic
    
    * - Increment for dev deploy.
    
    * - Add console.logs to try and troubleshoot why infoInternLinkUrl doesn't work in Matrix.
    
    * - Adjust data in manifest.json and component for internal link
    - Update default.data.json to test
    - Clean up server.js
    
    ---------
    
    Co-authored-by: Justin Rent <>

--- components/multicolumn-info-section/server.jsx
+++ components/multicolumn-info-section/server.jsx
@@ -4,9 +4,20 @@ import basicAssetUri from "../../packages/utils/basicAssetUri";
 
 export default async (args, info) => {
   const { ctx } = info;
+  /* Button in second column, with infoText */
+  const { infoInternalUrl } = args.colTwo.buttonConfiguration;
   const { image } = args.colThree.imageConfiguration;
+  /* Button in third column, inside InfoBox */
   const { internalUrl } = args.colThree.buttonConfiguration;
 
+  /* Button in second column, with infoText */
+  let infoLinkUrl = null;
+  if (infoInternalUrl) {
+    infoLinkUrl = await basicAssetUri(ctx, infoInternalUrl);
+  }
+  const infoInternalLinkUrl = infoLinkUrl?.url;
+
+  /* Button in third column, inside InfoBox */
   let linkUrl = null;
   if (internalUrl) {
     linkUrl = await basicAssetUri(ctx, internalUrl);
@@ -22,8 +33,10 @@ export default async (args, info) => {
   const renderProps = {
     ...args,
     imageData,
+    infoInternalLinkUrl,
     internalLinkUrl,
   };
+
   return renderComponent({
     Component,
     componentName: "multicolumn-info-section",
